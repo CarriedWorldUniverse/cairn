CarriedWorldUniverse/cairn

commit a8d2f73db7eae6642429d1fddb28ab2f6fb03844

Improve listing performance by using go-git (#6478)
* Use go-git for tree reading and commit info lookup.

Signed-off-by: Filip Navara <navara@emclient.com>

* Use TreeEntry.IsRegular() instead of ObjectType that was removed.

Signed-off-by: Filip Navara <navara@emclient.com>

* Use the treePath to optimize commit info search.

Signed-off-by: Filip Navara <navara@emclient.com>

* Extract the latest commit at treePath along with the other commits.

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix listing commit info for a directory that was created in one commit and never modified after.

Signed-off-by: Filip Navara <navara@emclient.com>

* Avoid nearly all external 'git' invocations when doing directory listing (.editorconfig code path is still hit).

Signed-off-by: Filip Navara <navara@emclient.com>

* Use go-git for reading blobs.

Signed-off-by: Filip Navara <navara@emclient.com>

* Make SHA1 type alias for plumbing.Hash in go-git.

Signed-off-by: Filip Navara <navara@emclient.com>

* Make Signature type alias for object.Signature in go-git.

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix GetCommitsInfo for repository with only one commit.

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix PGP signature verification.

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix issues with walking commit graph across merges.

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix typo in condition.

Signed-off-by: Filip Navara <navara@emclient.com>

* Speed up loading branch list by keeping the repository reference (and thus all the loaded packfile indexes).

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix lising submodules.

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix build

Signed-off-by: Filip Navara <navara@emclient.com>

* Add back commit cache because of name-rev

Signed-off-by: Filip Navara <navara@emclient.com>

* Fix tests

modules/git/commit_info.go
@@ -5,325 +5,237 @@
 package git
 
 import (
-	"bufio"
-	"context"
-	"fmt"
-	"os/exec"
-	"path"
-	"runtime"
-	"strconv"
-	"strings"
-	"sync"
-	"time"
+	"github.com/emirpasic/gods/trees/binaryheap"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
-
-const (
-	// parameters for searching for commit infos. If the untargeted search has
-	// not found any entries in the past 5 commits, and 12 or fewer entries
-	// remain, then we'll just let the targeted-searching threads finish off,
-	// and stop the untargeted search to not interfere.
-	deferToTargetedSearchColdStreak          = 5
-	deferToTargetedSearchNumRemainingEntries = 12
-)
-
-// getCommitsInfoState shared state while getting commit info for entries
-type getCommitsInfoState struct {
-	lock sync.Mutex
-	/* read-only fields, can be read without the mutex */
-	// entries and entryPaths are read-only after initialization, so they can
-	// safely be read without the mutex
-	entries []*TreeEntry
-	// set of filepaths to get info for
-	entryPaths map[string]struct{}
-	treePath   string
-	headCommit *Commit
-
-	/* mutable fields, must hold mutex to read or write */
-	// map from filepath to commit
-	commits map[string]*Commit
-	// set of filepaths that have been or are being searched for in a target search
-	targetedPaths map[string]struct{}
-}
-
-func (state *getCommitsInfoState) numRemainingEntries() int {
-	state.lock.Lock()
-	defer state.lock.Unlock()
-	return len(state.entries) - len(state.commits)
-}
-
-// getTargetEntryPath Returns the next path for a targeted-searching thread to
-// search for, or returns the empty string if nothing left to search for
-func (state *getCommitsInfoState) getTargetedEntryPath() string {
-	var targetedEntryPath string
-	state.lock.Lock()
-	defer state.lock.Unlock()
-	for _, entry := range state.entries {
-		entryPath := path.Join(state.treePath, entry.Name())
-		if _, ok := state.commits[entryPath]; ok {
-			continue
-		} else if _, ok = state.targetedPaths[entryPath]; ok {
-			continue
-		}
-		targetedEntryPath = entryPath
-		state.targetedPaths[entryPath] = struct{}{}
-		break
-	}
-	return targetedEntryPath
-}
-
-// repeatedly perform targeted searches for unpopulated entries
-func targetedSearch(state *getCommitsInfoState, done chan error, cache LastCommitCache) {
-	for {
-		entryPath := state.getTargetedEntryPath()
-		if len(entryPath) == 0 {
-			done <- nil
-			return
-		}
-		if cache != nil {
-			commit, err := cache.Get(state.headCommit.repo.Path, state.headCommit.ID.String(), entryPath)
-			if err == nil && commit != nil {
-				state.update(entryPath, commit)
-				continue
-			}
-		}
-		command := NewCommand("rev-list", "-1", state.headCommit.ID.String(), "--", entryPath)
-		output, err := command.RunInDir(state.headCommit.repo.Path)
-		if err != nil {
-			done <- err
-			return
-		}
-		id, err := NewIDFromString(strings.TrimSpace(output))
-		if err != nil {
-			done <- err
-			return
-		}
-		commit, err := state.headCommit.repo.getCommit(id)
-		if err != nil {
-			done <- err
-			return
-		}
-		state.update(entryPath, commit)
-		if cache != nil {
-			cache.Put(state.headCommit.repo.Path, state.headCommit.ID.String(), entryPath, commit)
-		}
-	}
-}
-
-func initGetCommitInfoState(entries Entries, headCommit *Commit, treePath string) *getCommitsInfoState {
-	entryPaths := make(map[string]struct{}, len(entries))
-	for _, entry := range entries {
-		entryPaths[path.Join(treePath, entry.Name())] = struct{}{}
-	}
-	if treePath = path.Clean(treePath); treePath == "." {
-		treePath = ""
-	}
-	return &getCommitsInfoState{
-		entries:       entries,
-		entryPaths:    entryPaths,
-		commits:       make(map[string]*Commit, len(entries)),
-		targetedPaths: make(map[string]struct{}, len(entries)),
-		treePath:      treePath,
-		headCommit:    headCommit,
-	}
-}
 
 // GetCommitsInfo gets information of all commits that are corresponding to these entries
-func (tes Entries) GetCommitsInfo(commit *Commit, treePath string, cache LastCommitCache) ([][]interface{}, error) {
-	state := initGetCommitInfoState(tes, commit, treePath)
-	if err := getCommitsInfo(state, cache); err != nil {
-		return nil, err
+func (tes Entries) GetCommitsInfo(commit *Commit, treePath string, cache LastCommitCache) ([][]interface{}, *Commit, error) {
+	entryPaths := make([]string, len(tes)+1)
+	// Get the commit for the treePath itself
+	entryPaths[0] = ""
+	for i, entry := range tes {
+		entryPaths[i+1] = entry.Name()
 	}
-	if len(state.commits) < len(state.entryPaths) {
-		return nil, fmt.Errorf("could not find commits for all entries")
+
+	c, err := commit.repo.gogitRepo.CommitObject(plumbing.Hash(commit.ID))
+	if err != nil {
+		return nil, nil, err
 	}
+
+	revs, err := getLastCommitForPaths(c, treePath, entryPaths)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	commit.repo.gogitStorage.Close()
 
 	commitsInfo := make([][]interface{}, len(tes))
 	for i, entry := range tes {
-		commit, ok := state.commits[path.Join(treePath, entry.Name())]
-		if !ok {
-			return nil, fmt.Errorf("could not find commit for %s", entry.Name())
-		}
-		switch entry.Type {
-		case ObjectCommit:
-			subModuleURL := ""
-			if subModule, err := state.headCommit.GetSubModule(entry.Name()); err != nil {
-				return nil, err
-			} else if subModule != nil {
-				subModuleURL = subModule.URL
-			}
-			subModuleFile := NewSubModuleFile(commit, subModuleURL, entry.ID.String())
-			commitsInfo[i] = []interface{}{entry, subModuleFile}
-		default:
-			commitsInfo[i] = []interface{}{entry, commit}
-		}
-	}
-	return commitsInfo, nil
-}
-
-func (state *getCommitsInfoState) cleanEntryPath(rawEntryPath string) (string, error) {
-	if rawEntryPath[0] == '"' {
-		var err error
-		rawEntryPath, err = strconv.Unquote(rawEntryPath)
-		if err != nil {
-			return rawEntryPath, err
-		}
-	}
-	var entryNameStartIndex int
-	if len(state.treePath) > 0 {
-		entryNameStartIndex = len(state.treePath) + 1
-	}
-
-	if index := strings.IndexByte(rawEntryPath[entryNameStartIndex:], '/'); index >= 0 {
-		return rawEntryPath[:entryNameStartIndex+index], nil
-	}
-	return rawEntryPath, nil
-}
-
-// update report that the given path was last modified by the given commit.
-// Returns whether state.commits was updated
-func (state *getCommitsInfoState) update(entryPath string, commit *Commit) bool {
-	if _, ok := state.entryPaths[entryPath]; !ok {
-		return false
-	}
-
-	var updated bool
-	state.lock.Lock()
-	defer state.lock.Unlock()
-	if _, ok := state.commits[entryPath]; !ok {
-		state.commits[entryPath] = commit
-		updated = true
-	}
-	return updated
-}
-
-const getCommitsInfoPretty = "--pretty=format:%H %ct %s"
-
-func getCommitsInfo(state *getCommitsInfoState, cache LastCommitCache) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
-	defer cancel()
-
-	args := []string{"log", state.headCommit.ID.String(), getCommitsInfoPretty, "--name-status", "-c"}
-	if len(state.treePath) > 0 {
-		args = append(args, "--", state.treePath)
-	}
-	cmd := exec.CommandContext(ctx, "git", args...)
-	cmd.Dir = state.headCommit.repo.Path
-
-	readCloser, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	// it's okay to ignore the error returned by cmd.Wait(); we expect the
-	// subprocess to sometimes have a non-zero exit status, since we may
-	// prematurely close stdout, resulting in a broken pipe.
-	defer cmd.Wait()
-
-	numThreads := runtime.NumCPU()
-	done := make(chan error, numThreads)
-	for i := 0; i < numThreads; i++ {
-		go targetedSearch(state, done, cache)
-	}
-
-	scanner := bufio.NewScanner(readCloser)
-	err = state.processGitLogOutput(scanner)
-
-	// it is important that we close stdout here; if we do not close
-	// stdout, the subprocess will keep running, and the deffered call
-	// cmd.Wait() may block for a long time.
-	if closeErr := readCloser.Close(); closeErr != nil && err == nil {
-		err = closeErr
-	}
-
-	for i := 0; i < numThreads; i++ {
-		doneErr := <-done
-		if doneErr != nil && err == nil {
-			err = doneErr
-		}
-	}
-	return err
-}
-
-func (state *getCommitsInfoState) processGitLogOutput(scanner *bufio.Scanner) error {
-	// keep a local cache of seen paths to avoid acquiring a lock for paths
-	// we've already seen
-	seenPaths := make(map[string]struct{}, len(state.entryPaths))
-	// number of consecutive commits without any finds
-	coldStreak := 0
-	var commit *Commit
-	var err error
-	for scanner.Scan() {
-		line := scanner.Text()
-		if len(line) == 0 { // in-between commits
-			numRemainingEntries := state.numRemainingEntries()
-			if numRemainingEntries == 0 {
-				break
-			}
-			if coldStreak >= deferToTargetedSearchColdStreak &&
-				numRemainingEntries <= deferToTargetedSearchNumRemainingEntries {
-				// stop this untargeted search, and let the targeted-search threads
-				// finish the work
-				break
-			}
-			continue
-		}
-		if line[0] >= 'A' && line[0] <= 'X' { // a file was changed by the current commit
-			// look for the last tab, since for copies (C) and renames (R) two
-			// filenames are printed: src, then dest
-			tabIndex := strings.LastIndexByte(line, '\t')
-			if tabIndex < 1 {
-				return fmt.Errorf("misformatted line: %s", line)
-			}
-			entryPath, err := state.cleanEntryPath(line[tabIndex+1:])
-			if err != nil {
-				return err
-			}
-			if _, ok := seenPaths[entryPath]; !ok {
-				if state.update(entryPath, commit) {
-					coldStreak = 0
+		if rev, ok := revs[entry.Name()]; ok {
+			entryCommit := convertCommit(rev)
+			if entry.IsSubModule() {
+				subModuleURL := ""
+				if subModule, err := commit.GetSubModule(entry.Name()); err != nil {
+					return nil, nil, err
+				} else if subModule != nil {
+					subModuleURL = subModule.URL
 				}
-				seenPaths[entryPath] = struct{}{}
+				subModuleFile := NewSubModuleFile(entryCommit, subModuleURL, entry.ID.String())
+				commitsInfo[i] = []interface{}{entry, subModuleFile}
+			} else {
+				commitsInfo[i] = []interface{}{entry, entryCommit}
 			}
-			continue
+		} else {
+			commitsInfo[i] = []interface{}{entry, nil}
 		}
-
-		// a new commit
-		commit, err = parseCommitInfo(line)
-		if err != nil {
-			return err
-		}
-		coldStreak++
 	}
-	return scanner.Err()
+
+	// Retrieve the commit for the treePath itself (see above). We basically
+	// get it for free during the tree traversal and it's used for listing
+	// pages to display information about newest commit for a given path.
+	var treeCommit *Commit
+	if rev, ok := revs[""]; ok {
+		treeCommit = convertCommit(rev)
+	}
+	return commitsInfo, treeCommit, nil
 }
 
-// parseCommitInfo parse a commit from a line of `git log` output. Expects the
-// line to be formatted according to getCommitsInfoPretty.
-func parseCommitInfo(line string) (*Commit, error) {
-	if len(line) < 43 {
-		return nil, fmt.Errorf("invalid git output: %s", line)
-	}
-	ref, err := NewIDFromString(line[:40])
+type commitAndPaths struct {
+	commit *object.Commit
+	// Paths that are still on the branch represented by commit
+	paths []string
+	// Set of hashes for the paths
+	hashes map[string]plumbing.Hash
+}
+
+func getCommitTree(c *object.Commit, treePath string) (*object.Tree, error) {
+	tree, err := c.Tree()
 	if err != nil {
 		return nil, err
 	}
-	spaceIndex := strings.IndexByte(line[41:], ' ')
-	if spaceIndex < 0 {
-		return nil, fmt.Errorf("invalid git output: %s", line)
+
+	// Optimize deep traversals by focusing only on the specific tree
+	if treePath != "" {
+		tree, err = tree.Tree(treePath)
+		if err != nil {
+			return nil, err
+		}
 	}
-	unixSeconds, err := strconv.Atoi(line[41 : 41+spaceIndex])
+
+	return tree, nil
+}
+
+func getFullPath(treePath, path string) string {
+	if treePath != "" {
+		if path != "" {
+			return treePath + "/" + path
+		}
+		return treePath
+	}
+	return path
+}
+
+func getFileHashes(c *object.Commit, treePath string, paths []string) (map[string]plumbing.Hash, error) {
+	tree, err := getCommitTree(c, treePath)
+	if err == object.ErrDirectoryNotFound {
+		// The whole tree didn't exist, so return empty map
+		return make(map[string]plumbing.Hash), nil
+	}
 	if err != nil {
 		return nil, err
 	}
-	message := line[spaceIndex+42:]
-	return &Commit{
-		ID:            ref,
-		CommitMessage: message,
-		Committer: &Signature{
-			When: time.Unix(int64(unixSeconds), 0),
-		},
-	}, nil
+
+	hashes := make(map[string]plumbing.Hash)
+	for _, path := range paths {
+		if path != "" {
+			entry, err := tree.FindEntry(path)
+			if err == nil {
+				hashes[path] = entry.Hash
+			}
+		} else {
+			hashes[path] = tree.Hash
+		}
+	}
+
+	return hashes, nil
+}
+
+func getLastCommitForPaths(c *object.Commit, treePath string, paths []string) (map[string]*object.Commit, error) {
+	// We do a tree traversal with nodes sorted by commit time
+	seen := make(map[plumbing.Hash]bool)
+	heap := binaryheap.NewWith(func(a, b interface{}) int {
+		if a.(*commitAndPaths).commit.Committer.When.Before(b.(*commitAndPaths).commit.Committer.When) {
+			return 1
+		}
+		return -1
+	})
+
+	result := make(map[string]*object.Commit)
+	initialHashes, err := getFileHashes(c, treePath, paths)
+	if err != nil {
+		return nil, err
+	}
+
+	// Start search from the root commit and with full set of paths
+	heap.Push(&commitAndPaths{c, paths, initialHashes})
+
+	for {
+		cIn, ok := heap.Pop()
+		if !ok {
+			break
+		}
+		current := cIn.(*commitAndPaths)
+		currentID := current.commit.ID()
+
+		if seen[currentID] {
+			continue
+		}
+		seen[currentID] = true
+
+		// Load the parent commits for the one we are currently examining
+		numParents := current.commit.NumParents()
+		var parents []*object.Commit
+		for i := 0; i < numParents; i++ {
+			parent, err := current.commit.Parent(i)
+			if err != nil {
+				break
+			}
+			parents = append(parents, parent)
+		}
+
+		// Examine the current commit and set of interesting paths
+		numOfParentsWithPath := make([]int, len(current.paths))
+		pathChanged := make([]bool, len(current.paths))
+		parentHashes := make([]map[string]plumbing.Hash, len(parents))
+		for j, parent := range parents {
+			parentHashes[j], err = getFileHashes(parent, treePath, current.paths)
+			if err != nil {
+				break
+			}
+
+			for i, path := range current.paths {
+				if parentHashes[j][path] != plumbing.ZeroHash {
+					numOfParentsWithPath[i]++
+					if parentHashes[j][path] != current.hashes[path] {
+						pathChanged[i] = true
+					}
+				}
+			}
+		}
+
+		var remainingPaths []string
+		for i, path := range current.paths {
+			switch numOfParentsWithPath[i] {
+			case 0:
+				// The path didn't exist in any parent, so it must have been created by
+				// this commit. The results could already contain some newer change from
+				// different path, so don't override that.
+				if result[path] == nil {
+					result[path] = current.commit
+				}
+			case 1:
+				// The file is present on exactly one parent, so check if it was changed
+				// and save the revision if it did.
+				if pathChanged[i] {
+					if result[path] == nil {
+						result[path] = current.commit
+					}
+				} else {
+					remainingPaths = append(remainingPaths, path)
+				}
+			default:
+				// The file is present on more than one of the parent paths, so this is
+				// a merge. We have to examine all the parent trees to find out where
+				// the change occurred. pathChanged[i] would tell us that the file was
+				// changed during the merge, but it wouldn't tell us the relevant commit
+				// that introduced it.
+				remainingPaths = append(remainingPaths, path)
+			}
+		}
+
+		if len(remainingPaths) > 0 {
+			// Add the parent nodes along with remaining paths to the heap for further
+			// processing.
+			for j, parent := range parents {
+				if seen[parent.ID()] {
+					continue
+				}
+
+				// Combine remainingPath with paths available on the parent branch
+				// and make union of them
+				var remainingPathsForParent []string
+				for _, path := range remainingPaths {
+					if parentHashes[j][path] != plumbing.ZeroHash {
+						remainingPathsForParent = append(remainingPathsForParent, path)
+					}
+				}
+
+				heap.Push(&commitAndPaths{parent, remainingPathsForParent, parentHashes[j]})
+			}
+		}
+	}
+
+	return result, nil
 }

go.mod
@@ -32,7 +32,7 @@ require (
 	github.com/dgrijalva/jwt-go v0.0.0-20161101193935-9ed569b5d1ac
 	github.com/edsrzf/mmap-go v0.0.0-20170320065105-0bce6a688712 // indirect
 	github.com/elazarl/go-bindata-assetfs v0.0.0-20151224045452-57eb5e1fc594 // indirect
-	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/emirpasic/gods v1.12.0
 	github.com/etcd-io/bbolt v1.3.2 // indirect
 	github.com/ethantkoenig/rupture v0.0.0-20180203182544-0a76f03a811a
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
@@ -127,7 +127,7 @@ require (
 	gopkg.in/ldap.v3 v3.0.2
 	gopkg.in/macaron.v1 v1.3.2
 	gopkg.in/redis.v2 v2.3.2 // indirect
-	gopkg.in/src-d/go-billy.v4 v4.3.0 // indirect
+	gopkg.in/src-d/go-billy.v4 v4.3.0
 	gopkg.in/src-d/go-git.v4 v4.10.0
 	gopkg.in/testfixtures.v2 v2.5.0
 	mvdan.cc/xurls/v2 v2.0.0

integrations/api_repo_git_blobs_test.go
@@ -38,7 +38,7 @@ func TestAPIReposGitBlobs(t *testing.T) {
 	var gitBlobResponse api.GitBlobResponse
 	DecodeJSON(t, resp, &gitBlobResponse)
 	assert.NotNil(t, gitBlobResponse)
-	expectedContent := "Y29tbWl0IDY1ZjFiZjI3YmMzYmY3MGY2NDY1NzY1ODYzNWU2NjA5NGVkYmNiNGQKQXV0aG9yOiB1c2VyMSA8YWRkcmVzczFAZXhhbXBsZS5jb20+CkRhdGU6ICAgU3VuIE1hciAxOSAxNjo0Nzo1OSAyMDE3IC0wNDAwCgogICAgSW5pdGlhbCBjb21taXQKCmRpZmYgLS1naXQgYS9SRUFETUUubWQgYi9SRUFETUUubWQKbmV3IGZpbGUgbW9kZSAxMDA2NDQKaW5kZXggMDAwMDAwMC4uNGI0ODUxYQotLS0gL2Rldi9udWxsCisrKyBiL1JFQURNRS5tZApAQCAtMCwwICsxLDMgQEAKKyMgcmVwbzEKKworRGVzY3JpcHRpb24gZm9yIHJlcG8xClwgTm8gbmV3bGluZSBhdCBlbmQgb2YgZmlsZQo="
+	expectedContent := "dHJlZSAyYTJmMWQ0NjcwNzI4YTJlMTAwNDllMzQ1YmQ3YTI3NjQ2OGJlYWI2CmF1dGhvciB1c2VyMSA8YWRkcmVzczFAZXhhbXBsZS5jb20+IDE0ODk5NTY0NzkgLTA0MDAKY29tbWl0dGVyIEV0aGFuIEtvZW5pZyA8ZXRoYW50a29lbmlnQGdtYWlsLmNvbT4gMTQ4OTk1NjQ3OSAtMDQwMAoKSW5pdGlhbCBjb21taXQK"
 	assert.Equal(t, expectedContent, gitBlobResponse.Content)
 
 	// Tests a private repo with no token so will fail

models/error.go
@@ -874,21 +874,6 @@ func (err ErrUserDoesNotHaveAccessToRepo) Error() string {
 //  |______  / |__|  (____  /___|  /\___  >___|  /
 //         \/             \/     \/     \/     \/
 
-// ErrBranchNotExist represents a "BranchNotExist" kind of error.
-type ErrBranchNotExist struct {
-	Name string
-}
-
-// IsErrBranchNotExist checks if an error is a ErrBranchNotExist.
-func IsErrBranchNotExist(err error) bool {
-	_, ok := err.(ErrBranchNotExist)
-	return ok
-}
-
-func (err ErrBranchNotExist) Error() string {
-	return fmt.Sprintf("branch does not exist [name: %s]", err.Name)
-}
-
 // ErrBranchAlreadyExists represents an error that branch with such name already exists.
 type ErrBranchAlreadyExists struct {
 	BranchName string

models/pull.go
@@ -1,4 +1,5 @@
 // Copyright 2015 The Gogs Authors. All rights reserved.
+// Copyright 2019 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -165,8 +166,8 @@ func (pr *PullRequest) APIFormat() *api.PullRequest {
 
 func (pr *PullRequest) apiFormat(e Engine) *api.PullRequest {
 	var (
-		baseBranch *Branch
-		headBranch *Branch
+		baseBranch *git.Branch
+		headBranch *git.Branch
 		baseCommit *git.Commit
 		headCommit *git.Commit
 		err        error

models/repo_branch.go
@@ -1,4 +1,5 @@
 // Copyright 2016 The Gogs Authors. All rights reserved.
+// Copyright 2019 The Gitea Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -86,53 +87,24 @@ func (repo *Repository) DeleteLocalBranch(branchName string) error {
 	return deleteLocalBranch(repo.LocalCopyPath(), repo.DefaultBranch, branchName)
 }
 
-// Branch holds the branch information
-type Branch struct {
-	Path string
-	Name string
-}
-
-// GetBranchesByPath returns a branch by it's path
-func GetBranchesByPath(path string) ([]*Branch, error) {
-	gitRepo, err := git.OpenRepository(path)
-	if err != nil {
-		return nil, err
-	}
-
-	brs, err := gitRepo.GetBranches()
-	if err != nil {
-		return nil, err
-	}
-
-	branches := make([]*Branch, len(brs))
-	for i := range brs {
-		branches[i] = &Branch{
-			Path: path,
-			Name: brs[i],
-		}
-	}
-	return branches, nil
-}
-
 // CanCreateBranch returns true if repository meets the requirements for creating new branches.
 func (repo *Repository) CanCreateBranch() bool {
 	return !repo.IsMirror
 }
 
-// GetBranch returns a branch by it's name
-func (repo *Repository) GetBranch(branch string) (*Branch, error) {
-	if !git.IsBranchExist(repo.RepoPath(), branch) {
-		return nil, ErrBranchNotExist{branch}
+// GetBranch returns a branch by its name
+func (repo *Repository) GetBranch(branch string) (*git.Branch, error) {
+	gitRepo, err := git.OpenRepository(repo.RepoPath())
+	if err != nil {
+		return nil, err
 	}
-	return &Branch{
-		Path: repo.RepoPath(),
-		Name: branch,
-	}, nil
+
+	return gitRepo.GetBranch(branch)
 }
 
 // GetBranches returns all the branches of a repository
-func (repo *Repository) GetBranches() ([]*Branch, error) {
-	return GetBranchesByPath(repo.RepoPath())
+func (repo *Repository) GetBranches() ([]*git.Branch, error) {
+	return git.GetBranchesByPath(repo.RepoPath())
 }
 
 // CheckBranchName validates branch name with existing repository branches
@@ -256,13 +228,4 @@ func (repo *Repository) CreateNewBranchFromCommit(doer *User, commit, branchName
 	}
 
 	return nil
-}
-
-// GetCommit returns all the commits of a branch
-func (branch *Branch) GetCommit() (*git.Commit, error) {
-	gitRepo, err := git.OpenRepository(branch.Path)
-	if err != nil {
-		return nil, err
-	}
-	return gitRepo.GetBranchCommit(branch.Name)
 }

modules/context/repo.go
@@ -157,10 +157,11 @@ func (r *Repository) GetEditorconfig() (*editorconfig.Editorconfig, error) {
 	if treeEntry.Blob().Size() >= setting.UI.MaxDisplayFileSize {
 		return nil, git.ErrNotExist{ID: "", RelPath: ".editorconfig"}
 	}
-	reader, err := treeEntry.Blob().Data()
+	reader, err := treeEntry.Blob().DataAsync()
 	if err != nil {
 		return nil, err
 	}
+	defer reader.Close()
 	data, err := ioutil.ReadAll(reader)
 	if err != nil {
 		return nil, err

commit 0ea341f0a65b4a27c7c5b51b7dc7ee0f85aeecc4

Fix one performance/correctness regression in #6478 found on Rails repository. (#6686)
* Fix flaw in the commit history lookup that caused unnecessary traversal
when the repository contains a lot of merge commits.

Also return the merge commit as the changed one if the file or
directory was changed as part of the merge, eg. through conflict
resolution.

Signed-off-by: Filip Navara <filip.navara@gmail.com>

* Perform history simplification. If a file is present on multiple parents
in a merge commit follow only the first parent.

modules/git/commit_info.go
@@ -147,12 +147,6 @@ func getLastCommitForPaths(c *object.Commit, treePath string, paths []string) (m
 			break
 		}
 		current := cIn.(*commitAndPaths)
-		currentID := current.commit.ID()
-
-		if seen[currentID] {
-			continue
-		}
-		seen[currentID] = true
 
 		// Load the parent commits for the one we are currently examining
 		numParents := current.commit.NumParents()
@@ -166,8 +160,7 @@ func getLastCommitForPaths(c *object.Commit, treePath string, paths []string) (m
 		}
 
 		// Examine the current commit and set of interesting paths
-		numOfParentsWithPath := make([]int, len(current.paths))
-		pathChanged := make([]bool, len(current.paths))
+		pathUnchanged := make([]bool, len(current.paths))
 		parentHashes := make([]map[string]plumbing.Hash, len(parents))
 		for j, parent := range parents {
 			parentHashes[j], err = getFileHashes(parent, treePath, current.paths)
@@ -176,42 +169,32 @@ func getLastCommitForPaths(c *object.Commit, treePath string, paths []string) (m
 			}
 
 			for i, path := range current.paths {
-				if parentHashes[j][path] != plumbing.ZeroHash {
-					numOfParentsWithPath[i]++
-					if parentHashes[j][path] != current.hashes[path] {
-						pathChanged[i] = true
-					}
+				if parentHashes[j][path] == current.hashes[path] {
+					pathUnchanged[i] = true
 				}
 			}
 		}
 
 		var remainingPaths []string
 		for i, path := range current.paths {
-			switch numOfParentsWithPath[i] {
-			case 0:
-				// The path didn't exist in any parent, so it must have been created by
-				// this commit. The results could already contain some newer change from
-				// different path, so don't override that.
-				if result[path] == nil {
+			// The results could already contain some newer change for the same path,
+			// so don't override that and bail out on the file early.
+			if result[path] == nil {
+				if pathUnchanged[i] {
+					// The path existed with the same hash in at least one parent so it could
+					// not have been changed in this commit directly.
+					remainingPaths = append(remainingPaths, path)
+				} else {
+					// There are few possible cases how can we get here:
+					// - The path didn't exist in any parent, so it must have been created by
+					//   this commit.
+					// - The path did exist in the parent commit, but the hash of the file has
+					//   changed.
+					// - We are looking at a merge commit and the hash of the file doesn't
+					//   match any of the hashes being merged. This is more common for directories,
+					//   but it can also happen if a file is changed through conflict resolution.
 					result[path] = current.commit
 				}
-			case 1:
-				// The file is present on exactly one parent, so check if it was changed
-				// and save the revision if it did.
-				if pathChanged[i] {
-					if result[path] == nil {
-						result[path] = current.commit
-					}
-				} else {
-					remainingPaths = append(remainingPaths, path)
-				}
-			default:
-				// The file is present on more than one of the parent paths, so this is
-				// a merge. We have to examine all the parent trees to find out where
-				// the change occurred. pathChanged[i] would tell us that the file was
-				// changed during the merge, but it wouldn't tell us the relevant commit
-				// that introduced it.
-				remainingPaths = append(remainingPaths, path)
 			}
 		}
 
@@ -222,17 +205,29 @@ func getLastCommitForPaths(c *object.Commit, treePath string, paths []string) (m
 				if seen[parent.ID()] {
 					continue
 				}
+				seen[parent.ID()] = true
 
 				// Combine remainingPath with paths available on the parent branch
 				// and make union of them
 				var remainingPathsForParent []string
+				var newRemainingPaths []string
 				for _, path := range remainingPaths {
-					if parentHashes[j][path] != plumbing.ZeroHash {
+					if parentHashes[j][path] == current.hashes[path] {
 						remainingPathsForParent = append(remainingPathsForParent, path)
+					} else {
+						newRemainingPaths = append(newRemainingPaths, path)
 					}
 				}
 
-				heap.Push(&commitAndPaths{parent, remainingPathsForParent, parentHashes[j]})
+				if remainingPathsForParent != nil {
+					heap.Push(&commitAndPaths{parent, remainingPathsForParent, parentHashes[j]})
+				}
+
+				if len(newRemainingPaths) == 0 {
+					break
+				} else {
+					remainingPaths = newRemainingPaths
+				}
 			}
 		}
 	}