CarriedWorldUniverse/cairn

commit ca71d9159a1c170b4e894e495bf1810855a13f74

fix Dropzone.js integration (#7445) (#7448)

public/js/index.js
@@ -10,6 +10,9 @@ function htmlEncode(text) {
 var csrf;
 var suburl;
 
+// Disable Dropzone auto-discover because it's manually initialized
+Dropzone.autoDiscover = false;
+
 // Polyfill for IE9+ support (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from)
 if (!Array.from) {
     Array.from = (function () {
@@ -1972,13 +1975,11 @@ $(document).ready(function () {
     }
 
     // Dropzone
-    var $dropzone = $('#dropzone');
+    const $dropzone = $('#dropzone');
     if ($dropzone.length > 0) {
-        // Disable auto discover for all elements:
-        Dropzone.autoDiscover = false;
+        const filenameDict = {};
 
-        var filenameDict = {};
-        $dropzone.dropzone({
+        new Dropzone("#dropzone", {
             url: $dropzone.data('upload-url'),
             headers: {"X-Csrf-Token": csrf},
             maxFiles: $dropzone.data('max-file'),
@@ -2006,7 +2007,7 @@ $(document).ready(function () {
                         });
                     }
                 })
-            }
+            },
         });
     }
 

commit 550c8cebe09d80918d7a2b5bc6785a3ac2ae5db7

Fix an issue with some pages throwing 'not defined' js exceptions #7450 (#7453)
Fix an issue introduced by ca71d9159a1c170b4e894e495bf1810855a13f74

Signed-off-by: Gary Kim <gary@garykim.dev>

public/js/index.js
@@ -11,7 +11,9 @@ var csrf;
 var suburl;
 
 // Disable Dropzone auto-discover because it's manually initialized
-Dropzone.autoDiscover = false;
+if (typeof(Dropzone) !== "undefined") {
+    Dropzone.autoDiscover = false;
+}
 
 // Polyfill for IE9+ support (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from)
 if (!Array.from) {