CarriedWorldUniverse/cairn

commit d6ed3c47c4277fd850af5ae30db3e2951c6d868f

Fix public org members displayed too many informations (#20403)

templates/org/member/members.tmpl
@@ -29,28 +29,30 @@
 							{{end}}
 						</div>
 					</div>
-					<div class="ui three wide column center">
-						<div class="meta">
-							{{$.locale.Tr "org.members.member_role"}}
+					{{if not .PublicOnly}}
+						<div class="ui three wide column center">
+							<div class="meta">
+								{{$.locale.Tr "org.members.member_role"}}
+							</div>
+							<div class="meta">
+								<strong>{{if index $.MembersIsUserOrgOwner .ID}}{{svg "octicon-shield-lock"}} {{$.locale.Tr "org.members.owner"}}{{else}}{{$.locale.Tr "org.members.member"}}{{end}}</strong>
+							</div>
 						</div>
-						<div class="meta">
-							<strong>{{if index $.MembersIsUserOrgOwner .ID}}{{svg "octicon-shield-lock"}} {{$.locale.Tr "org.members.owner"}}{{else}}{{$.locale.Tr "org.members.member"}}{{end}}</strong>
+						<div class="ui two wide column center">
+							<div class="meta">
+								{{$.locale.Tr "admin.users.2fa"}}
+							</div>
+							<div class="meta">
+								<strong>
+									{{if index $.MembersTwoFaStatus .ID}}
+										<span class="text green">{{svg "octicon-check"}}</span>
+									{{else}}
+										{{svg "octicon-x"}}
+									{{end}}
+								</strong>
+							</div>
 						</div>
-					</div>
-					<div class="ui two wide column center">
-						<div class="meta">
-							{{$.locale.Tr "admin.users.2fa"}}
-						</div>
-						<div class="meta">
-							<strong>
-								{{if index $.MembersTwoFaStatus .ID}}
-									<span class="text green">{{svg "octicon-check"}}</span>
-								{{else}}
-									{{svg "octicon-x"}}
-								{{end}}
-							</strong>
-						</div>
-					</div>
+					{{end}}
 					<div class="ui three wide column">
 						<div class="text right">
 							{{if eq $.SignedUser.ID .ID}}

routers/web/org/members.go
@@ -45,6 +45,7 @@ func Members(ctx *context.Context) {
 		}
 		opts.PublicOnly = !isMember && !ctx.Doer.IsAdmin
 	}
+	ctx.Data["PublicOnly"] = opts.PublicOnly
 
 	total, err := organization.CountOrgMembers(opts)
 	if err != nil {

commit 68820c77e329c124d6ef57a588fd930e7ff6196f

Fix org members bug (#20489)
* Fix bug in public only org members list

bug was introduced in d6ed3c47c4

* Expanded org unit test

templates/org/member/members.tmpl
@@ -29,7 +29,7 @@
 							{{end}}
 						</div>
 					</div>
-					{{if not .PublicOnly}}
+					{{if not $.PublicOnly}}
 						<div class="ui three wide column center">
 							<div class="meta">
 								{{$.locale.Tr "org.members.member_role"}}

integrations/org_test.go
@@ -116,6 +116,24 @@ func TestPrivateOrg(t *testing.T) {
 	session.MakeRequest(t, req, http.StatusOK)
 }
 
+func TestOrgMembers(t *testing.T) {
+	defer prepareTestEnv(t)()
+
+	// not logged in user
+	req := NewRequest(t, "GET", "/org/org25/members")
+	MakeRequest(t, req, http.StatusOK)
+
+	// org member
+	session := loginUser(t, "user24")
+	req = NewRequest(t, "GET", "/org/org25/members")
+	session.MakeRequest(t, req, http.StatusOK)
+
+	// site admin
+	session = loginUser(t, "user1")
+	req = NewRequest(t, "GET", "/org/org25/members")
+	session.MakeRequest(t, req, http.StatusOK)
+}
+
 func TestOrgRestrictedUser(t *testing.T) {
 	defer prepareTestEnv(t)()
 