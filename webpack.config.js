jsoref/forgejo-forgejo

[API] Forgejo API /api/forgejo/v1

webpack.config.js
@@ -59,6 +59,10 @@ export default {
       fileURLToPath(new URL('web_src/fomantic/build/semantic.css', import.meta.url)),
       fileURLToPath(new URL('web_src/less/index.less', import.meta.url)),
     ],
+    forgejoswagger: [ // Forgejo swagger is OpenAPI 3.0.0 and has specific parameters
+	fileURLToPath(new URL('web_src/js/standalone/forgejo-swagger.js', import.meta.url)),
+	fileURLToPath(new URL('web_src/less/standalone/swagger.less', import.meta.url)),
+    ],
     swagger: [
       fileURLToPath(new URL('web_src/js/standalone/swagger.js', import.meta.url)),
       fileURLToPath(new URL('web_src/less/standalone/swagger.less', import.meta.url)),

CarriedWorldUniverse/cairn

(cherry picked from commit b7e9f80890f6f2997b04cd0753e3a771fa1654bd)
(cherry picked from commit d38e9108d137fe3bc5c6ed4db777f48eade72b7a)
Update semantic version according to specification
(cherry picked from commit 78b83a22bff10b4d53cc0991f549da7414c8581a)
Mise à jour de 'Makefile'
(cherry picked from commit dece5af58a3b5d89beea2490059a4ee0cdd85e64)
(cherry picked from commit b2b658c1b11e7b09c858c0e5baf85d3d4bd952ea)
(cherry picked from commit 07e58214f1e62efee44735472d27e36e5f4ee6d5)
[API] [SEMVER] replace number with version
[API] [SEMVER] [v1.20] less is replaced by css
(cherry picked from commit 622aaa48a839f695e8cc9eb8f4aca342b5fbeab1)
(cherry picked from commit 1da4a83fd19e42b3272f5283c2da562ab9dd6d77)
(cherry picked from commit 6530540ea91887f086989f32afa2d6a06b45eb6a)
(cherry picked from commit bb69380e19c66c9fbaa9cf59ef80bd4df5d37ad0)
(cherry picked from commit 721ecff3862159f3463f3da3bba919ccce250220)
(cherry picked from commit 26fa5ebbad13b344898e2cbba78426adebe12fb6)

webpack.config.js
@@ -60,7 +60,11 @@ export default {
       fileURLToPath(new URL('web_src/css/index.css', import.meta.url)),
     ],
     webcomponents: [
-      fileURLToPath(new URL('web_src/js/webcomponents/GiteaOriginUrl.js', import.meta.url)),
+	fileURLToPath(new URL('web_src/js/webcomponents/GiteaOriginUrl.js', import.meta.url)),
+    ],
+    forgejoswagger: [ // Forgejo swagger is OpenAPI 3.0.0 and has specific parameters
+	fileURLToPath(new URL('web_src/js/standalone/forgejo-swagger.js', import.meta.url)),
+	fileURLToPath(new URL('web_src/css/standalone/swagger.css', import.meta.url)),
     ],
     swagger: [
       fileURLToPath(new URL('web_src/js/standalone/swagger.js', import.meta.url)),