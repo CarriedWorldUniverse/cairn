CarriedWorldUniverse/cairn

commit aed356278a1043c09fc61c19ba1edc6c59077198

Use the type RefName for all the needed places and fix pull mirror sync bugs (#24634)
This PR replaces all string refName as a type `git.RefName` to make the
code more maintainable.

Fix #15367
Replaces #23070 
It also fixed a bug that tags are not sync because `git remote --prune
origin` will not remove local tags if remote removed.

We in fact should use `git fetch --prune --tags origin` but not `git
remote update origin` to do the sync.

Some answer from ChatGPT as ref.

> If the git fetch --prune --tags command is not working as expected,
there could be a few reasons why. Here are a few things to check:
> 
>Make sure that you have the latest version of Git installed on your
system. You can check the version by running git --version in your
terminal. If you have an outdated version, try updating Git and see if
that resolves the issue.
> 
>Check that your Git repository is properly configured to track the
remote repository's tags. You can check this by running git config
--get-all remote.origin.fetch and verifying that it includes
+refs/tags/*:refs/tags/*. If it does not, you can add it by running git
config --add remote.origin.fetch "+refs/tags/*:refs/tags/*".
> 
>Verify that the tags you are trying to prune actually exist on the
remote repository. You can do this by running git ls-remote --tags
origin to list all the tags on the remote repository.
> 
>Check if any local tags have been created that match the names of tags
on the remote repository. If so, these local tags may be preventing the
git fetch --prune --tags command from working properly. You can delete
local tags using the git tag -d command.

---------

Co-authored-by: delvh <dev.lh@web.de>

modules/git/ref.go
@@ -6,6 +6,8 @@ package git
 import (
 	"regexp"
 	"strings"
+
+	"code.gitea.io/gitea/modules/util"
 )
 
 const (
@@ -13,8 +15,6 @@ const (
 	RemotePrefix = "refs/remotes/"
 	// PullPrefix is the base directory of the pull information of git.
 	PullPrefix = "refs/pull/"
-
-	pullLen = len(PullPrefix)
 )
 
 // refNamePatternInvalid is regular expression with unallowed characters in git reference name
@@ -53,18 +53,31 @@ func (ref *Reference) Commit() (*Commit, error) {
 	return ref.repo.getCommit(ref.Object)
 }
 
-// ShortName returns the short name of the reference
-func (ref *Reference) ShortName() string {
-	return RefName(ref.Name).ShortName()
-}
-
 // RefGroup returns the group type of the reference
 func (ref *Reference) RefGroup() string {
 	return RefName(ref.Name).RefGroup()
 }
 
-// RefName represents a git reference name
+// ForPrefix special ref to create a pull request: refs/for/<target-branch>/<topic-branch>
+// or refs/for/<targe-branch> -o topic='<topic-branch>'
+const ForPrefix = "refs/for/"
+
+// TODO: /refs/for-review for suggest change interface
+
+// RefName represents a full git reference name
 type RefName string
+
+func RefNameFromBranch(shortName string) RefName {
+	return RefName(BranchPrefix + shortName)
+}
+
+func RefNameFromTag(shortName string) RefName {
+	return RefName(TagPrefix + shortName)
+}
+
+func (ref RefName) String() string {
+	return string(ref)
+}
 
 func (ref RefName) IsBranch() bool {
 	return strings.HasPrefix(string(ref), BranchPrefix)
@@ -74,20 +87,71 @@ func (ref RefName) IsTag() bool {
 	return strings.HasPrefix(string(ref), TagPrefix)
 }
 
+func (ref RefName) IsRemote() bool {
+	return strings.HasPrefix(string(ref), RemotePrefix)
+}
+
+func (ref RefName) IsPull() bool {
+	return strings.HasPrefix(string(ref), PullPrefix) && strings.IndexByte(string(ref)[len(PullPrefix):], '/') > -1
+}
+
+func (ref RefName) IsFor() bool {
+	return strings.HasPrefix(string(ref), ForPrefix)
+}
+
+func (ref RefName) nameWithoutPrefix(prefix string) string {
+	if strings.HasPrefix(string(ref), prefix) {
+		return strings.TrimPrefix(string(ref), prefix)
+	}
+	return ""
+}
+
+// TagName returns simple tag name if it's an operation to a tag
+func (ref RefName) TagName() string {
+	return ref.nameWithoutPrefix(TagPrefix)
+}
+
+// BranchName returns simple branch name if it's an operation to branch
+func (ref RefName) BranchName() string {
+	return ref.nameWithoutPrefix(BranchPrefix)
+}
+
+// PullName returns the pull request name part of refs like refs/pull/<pull_name>/head
+func (ref RefName) PullName() string {
+	refName := string(ref)
+	lastIdx := strings.LastIndexByte(refName[len(PullPrefix):], '/')
+	if strings.HasPrefix(refName, PullPrefix) && lastIdx > -1 {
+		return refName[len(PullPrefix) : lastIdx+len(PullPrefix)]
+	}
+	return ""
+}
+
+// ForBranchName returns the branch name part of refs like refs/for/<branch_name>
+func (ref RefName) ForBranchName() string {
+	return ref.nameWithoutPrefix(ForPrefix)
+}
+
+func (ref RefName) RemoteName() string {
+	return ref.nameWithoutPrefix(RemotePrefix)
+}
+
 // ShortName returns the short name of the reference name
 func (ref RefName) ShortName() string {
 	refName := string(ref)
-	if strings.HasPrefix(refName, BranchPrefix) {
-		return strings.TrimPrefix(refName, BranchPrefix)
+	if ref.IsBranch() {
+		return ref.BranchName()
 	}
-	if strings.HasPrefix(refName, TagPrefix) {
-		return strings.TrimPrefix(refName, TagPrefix)
+	if ref.IsTag() {
+		return ref.TagName()
 	}
-	if strings.HasPrefix(refName, RemotePrefix) {
-		return strings.TrimPrefix(refName, RemotePrefix)
+	if ref.IsRemote() {
+		return ref.RemoteName()
 	}
-	if strings.HasPrefix(refName, PullPrefix) && strings.IndexByte(refName[pullLen:], '/') > -1 {
-		return refName[pullLen : strings.IndexByte(refName[pullLen:], '/')+pullLen]
+	if ref.IsPull() {
+		return ref.PullName()
+	}
+	if ref.IsFor() {
+		return ref.ForBranchName()
 	}
 
 	return refName
@@ -95,18 +159,37 @@ func (ref RefName) ShortName() string {
 
 // RefGroup returns the group type of the reference
 func (ref RefName) RefGroup() string {
-	refName := string(ref)
-	if strings.HasPrefix(refName, BranchPrefix) {
+	if ref.IsBranch() {
 		return "heads"
 	}
-	if strings.HasPrefix(refName, TagPrefix) {
+	if ref.IsTag() {
 		return "tags"
 	}
-	if strings.HasPrefix(refName, RemotePrefix) {
+	if ref.IsRemote() {
 		return "remotes"
 	}
-	if strings.HasPrefix(refName, PullPrefix) && strings.IndexByte(refName[pullLen:], '/') > -1 {
+	if ref.IsPull() {
 		return "pull"
 	}
+	if ref.IsFor() {
+		return "for"
+	}
 	return ""
+}
+
+// RefURL returns the absolute URL for a ref in a repository
+func RefURL(repoURL, ref string) string {
+	refFullName := RefName(ref)
+	refName := util.PathEscapeSegments(refFullName.ShortName())
+	switch {
+	case refFullName.IsBranch():
+		return repoURL + "/src/branch/" + refName
+	case refFullName.IsTag():
+		return repoURL + "/src/tag/" + refName
+	case !IsValidSHAPattern(ref):
+		// assume they mean a branch
+		return repoURL + "/src/branch/" + refName
+	default:
+		return repoURL + "/src/commit/" + refName
+	}
 }

routers/api/actions/runner/utils.go
@@ -98,14 +98,8 @@ func generateTaskContext(t *actions_model.ActionTask) *structpb.Struct {
 		baseRef = pullPayload.PullRequest.Base.Ref
 		headRef = pullPayload.PullRequest.Head.Ref
 	}
-	refPrefix, refName := git.SplitRefName(t.Job.Run.Ref)
-	refType := ""
-	switch refPrefix {
-	case git.BranchPrefix:
-		refType = "branch"
-	case git.TagPrefix:
-		refType = "tag"
-	}
+	refName := git.RefName(t.Job.Run.Ref)
+	refType := refName.RefGroup()
 
 	taskContext, _ := structpb.NewStruct(map[string]interface{}{
 		// standard contexts, see https://docs.github.com/en/actions/learn-github-actions/contexts#github-context

services/actions/notifier.go
@@ -351,9 +351,9 @@ func (n *actionsNotifier) NotifyPushCommits(ctx context.Context, pusher *user_mo
 	}
 
 	newNotifyInput(repo, pusher, webhook_module.HookEventPush).
-		WithRef(opts.RefFullName).
+		WithRef(opts.RefFullName.String()).
 		WithPayload(&api.PushPayload{
-			Ref:        opts.RefFullName,
+			Ref:        opts.RefFullName.String(),
 			Before:     opts.OldCommitID,
 			After:      opts.NewCommitID,
 			CompareURL: setting.AppURL + commits.CompareURL,
@@ -366,37 +366,35 @@ func (n *actionsNotifier) NotifyPushCommits(ctx context.Context, pusher *user_mo
 		Notify(ctx)
 }
 
-func (n *actionsNotifier) NotifyCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName, refID string) {
+func (n *actionsNotifier) NotifyCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName, refID string) {
 	ctx = withMethod(ctx, "NotifyCreateRef")
 
 	apiPusher := convert.ToUser(ctx, pusher, nil)
 	apiRepo := convert.ToRepo(ctx, repo, perm_model.AccessModeNone)
-	refName := git.RefEndName(refFullName)
 
 	newNotifyInput(repo, pusher, webhook_module.HookEventCreate).
-		WithRef(refName).
+		WithRef(refFullName.ShortName()). // FIXME: should we use a full ref name
 		WithPayload(&api.CreatePayload{
-			Ref:     refName,
+			Ref:     refFullName.ShortName(),
 			Sha:     refID,
-			RefType: refType,
+			RefType: refFullName.RefGroup(),
 			Repo:    apiRepo,
 			Sender:  apiPusher,
 		}).
 		Notify(ctx)
 }
 
-func (n *actionsNotifier) NotifyDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName string) {
+func (n *actionsNotifier) NotifyDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName) {
 	ctx = withMethod(ctx, "NotifyDeleteRef")
 
 	apiPusher := convert.ToUser(ctx, pusher, nil)
 	apiRepo := convert.ToRepo(ctx, repo, perm_model.AccessModeNone)
-	refName := git.RefEndName(refFullName)
 
 	newNotifyInput(repo, pusher, webhook_module.HookEventDelete).
-		WithRef(refName).
+		WithRef(refFullName.ShortName()). // FIXME: should we use a full ref name
 		WithPayload(&api.DeletePayload{
-			Ref:        refName,
-			RefType:    refType,
+			Ref:        refFullName.ShortName(),
+			RefType:    refFullName.RefGroup(),
 			PusherType: api.PusherTypeUser,
 			Repo:       apiRepo,
 			Sender:     apiPusher,
@@ -415,9 +413,9 @@ func (n *actionsNotifier) NotifySyncPushCommits(ctx context.Context, pusher *use
 	}
 
 	newNotifyInput(repo, pusher, webhook_module.HookEventPush).
-		WithRef(opts.RefFullName).
+		WithRef(opts.RefFullName.String()).
 		WithPayload(&api.PushPayload{
-			Ref:          opts.RefFullName,
+			Ref:          opts.RefFullName.String(),
 			Before:       opts.OldCommitID,
 			After:        opts.NewCommitID,
 			CompareURL:   setting.AppURL + commits.CompareURL,
@@ -431,14 +429,14 @@ func (n *actionsNotifier) NotifySyncPushCommits(ctx context.Context, pusher *use
 		Notify(ctx)
 }
 
-func (n *actionsNotifier) NotifySyncCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName, refID string) {
+func (n *actionsNotifier) NotifySyncCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName, refID string) {
 	ctx = withMethod(ctx, "NotifySyncCreateRef")
-	n.NotifyCreateRef(ctx, pusher, repo, refType, refFullName, refID)
+	n.NotifyCreateRef(ctx, pusher, repo, refFullName, refID)
 }
 
-func (n *actionsNotifier) NotifySyncDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName string) {
+func (n *actionsNotifier) NotifySyncDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName) {
 	ctx = withMethod(ctx, "NotifySyncDeleteRef")
-	n.NotifyDeleteRef(ctx, pusher, repo, refType, refFullName)
+	n.NotifyDeleteRef(ctx, pusher, repo, refFullName)
 }
 
 func (n *actionsNotifier) NotifyNewRelease(ctx context.Context, rel *repo_model.Release) {

services/webhook/notifier.go
@@ -596,7 +596,7 @@ func (m *webhookNotifier) NotifyPushCommits(ctx context.Context, pusher *user_mo
 	}
 
 	if err := PrepareWebhooks(ctx, EventSource{Repository: repo}, webhook_module.HookEventPush, &api.PushPayload{
-		Ref:          opts.RefFullName,
+		Ref:          opts.RefFullName.String(),
 		Before:       opts.OldCommitID,
 		After:        opts.NewCommitID,
 		CompareURL:   setting.AppURL + commits.CompareURL,
@@ -747,15 +747,15 @@ func (m *webhookNotifier) NotifyPullReviewRequest(ctx context.Context, doer *use
 	}
 }
 
-func (m *webhookNotifier) NotifyCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName, refID string) {
+func (m *webhookNotifier) NotifyCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName, refID string) {
 	apiPusher := convert.ToUser(ctx, pusher, nil)
 	apiRepo := convert.ToRepo(ctx, repo, perm.AccessModeNone)
-	refName := git.RefEndName(refFullName)
+	refName := refFullName.ShortName()
 
 	if err := PrepareWebhooks(ctx, EventSource{Repository: repo}, webhook_module.HookEventCreate, &api.CreatePayload{
-		Ref:     refName,
+		Ref:     refName, // FIXME: should it be a full ref name?
 		Sha:     refID,
-		RefType: refType,
+		RefType: refFullName.RefGroup(),
 		Repo:    apiRepo,
 		Sender:  apiPusher,
 	}); err != nil {
@@ -784,19 +784,19 @@ func (m *webhookNotifier) NotifyPullRequestSynchronized(ctx context.Context, doe
 	}
 }
 
-func (m *webhookNotifier) NotifyDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName string) {
+func (m *webhookNotifier) NotifyDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName) {
 	apiPusher := convert.ToUser(ctx, pusher, nil)
 	apiRepo := convert.ToRepo(ctx, repo, perm.AccessModeNone)
-	refName := git.RefEndName(refFullName)
+	refName := refFullName.ShortName()
 
 	if err := PrepareWebhooks(ctx, EventSource{Repository: repo}, webhook_module.HookEventDelete, &api.DeletePayload{
-		Ref:        refName,
-		RefType:    refType,
+		Ref:        refName, // FIXME: should it be a full ref name?
+		RefType:    refFullName.RefGroup(),
 		PusherType: api.PusherTypeUser,
 		Repo:       apiRepo,
 		Sender:     apiPusher,
 	}); err != nil {
-		log.Error("PrepareWebhooks.(delete %s): %v", refType, err)
+		log.Error("PrepareWebhooks.(delete %s): %v", refFullName.RefGroup(), err)
 	}
 }
 
@@ -838,7 +838,7 @@ func (m *webhookNotifier) NotifySyncPushCommits(ctx context.Context, pusher *use
 	}
 
 	if err := PrepareWebhooks(ctx, EventSource{Repository: repo}, webhook_module.HookEventPush, &api.PushPayload{
-		Ref:          opts.RefFullName,
+		Ref:          opts.RefFullName.String(),
 		Before:       opts.OldCommitID,
 		After:        opts.NewCommitID,
 		CompareURL:   setting.AppURL + commits.CompareURL,
@@ -853,12 +853,12 @@ func (m *webhookNotifier) NotifySyncPushCommits(ctx context.Context, pusher *use
 	}
 }
 
-func (m *webhookNotifier) NotifySyncCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName, refID string) {
-	m.NotifyCreateRef(ctx, pusher, repo, refType, refFullName, refID)
+func (m *webhookNotifier) NotifySyncCreateRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName, refID string) {
+	m.NotifyCreateRef(ctx, pusher, repo, refFullName, refID)
 }
 
-func (m *webhookNotifier) NotifySyncDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refType, refFullName string) {
-	m.NotifyDeleteRef(ctx, pusher, repo, refType, refFullName)
+func (m *webhookNotifier) NotifySyncDeleteRef(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, refFullName git.RefName) {
+	m.NotifyDeleteRef(ctx, pusher, repo, refFullName)
 }
 
 func (m *webhookNotifier) NotifyPackageCreate(ctx context.Context, doer *user_model.User, pd *packages_model.PackageDescriptor) {

modules/git/utils.go
@@ -10,8 +10,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-
-	"code.gitea.io/gitea/modules/util"
 )
 
 // ObjectCache provides thread-safe cache operations.
@@ -76,48 +74,6 @@ func ConcatenateError(err error, stderr string) error {
 		return err
 	}
 	return fmt.Errorf("%w - %s", err, stderr)
-}
-
-// RefEndName return the end name of a ref name
-func RefEndName(refStr string) string {
-	if strings.HasPrefix(refStr, BranchPrefix) {
-		return refStr[len(BranchPrefix):]
-	}
-
-	if strings.HasPrefix(refStr, TagPrefix) {
-		return refStr[len(TagPrefix):]
-	}
-
-	return refStr
-}
-
-// RefURL returns the absolute URL for a ref in a repository
-func RefURL(repoURL, ref string) string {
-	refName := util.PathEscapeSegments(RefEndName(ref))
-	switch {
-	case strings.HasPrefix(ref, BranchPrefix):
-		return repoURL + "/src/branch/" + refName
-	case strings.HasPrefix(ref, TagPrefix):
-		return repoURL + "/src/tag/" + refName
-	case !IsValidSHAPattern(ref):
-		// assume they mean a branch
-		return repoURL + "/src/branch/" + refName
-	default:
-		return repoURL + "/src/commit/" + refName
-	}
-}
-
-// SplitRefName splits a full refname to reftype and simple refname
-func SplitRefName(refStr string) (string, string) {
-	if strings.HasPrefix(refStr, BranchPrefix) {
-		return BranchPrefix, refStr[len(BranchPrefix):]
-	}
-
-	if strings.HasPrefix(refStr, TagPrefix) {
-		return TagPrefix, refStr[len(TagPrefix):]
-	}
-
-	return "", refStr
 }
 
 // ParseBool returns the boolean value represented by the string as per git's git_config_bool

modules/notification/base/notifier.go
@@ -10,6 +10,7 @@ import (
 	packages_model "code.gitea.io/gitea/models/packages"
 	repo_model "code.gitea.io/gitea/models/repo"
 	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/git"
 	"code.gitea.io/gitea/modules/repository"
 )
 
@@ -54,11 +55,11 @@ type Notifier interface {
 	NotifyUpdateRelease(ctx context.Context, doer *user_model.User, rel *repo_model.Release)
 	NotifyDeleteRelease(ctx context.Context, doer *user_model.User, rel *repo_model.Release)
 	NotifyPushCommits(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, opts *repository.PushUpdateOptions, commits *repository.PushCommits)
-	NotifyCreateRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refType, refFullName, refID string)
-	NotifyDeleteRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refType, refFullName string)
+	NotifyCreateRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refFullName git.RefName, refID string)
+	NotifyDeleteRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refFullName git.RefName)
 	NotifySyncPushCommits(ctx context.Context, pusher *user_model.User, repo *repo_model.Repository, opts *repository.PushUpdateOptions, commits *repository.PushCommits)
-	NotifySyncCreateRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refType, refFullName, refID string)
-	NotifySyncDeleteRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refType, refFullName string)
+	NotifySyncCreateRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refFullName git.RefName, refID string)
+	NotifySyncDeleteRef(ctx context.Context, doer *user_model.User, repo *repo_model.Repository, refFullName git.RefName)
 	NotifyRepoPendingTransfer(ctx context.Context, doer, newOwner *user_model.User, repo *repo_model.Repository)
 	NotifyPackageCreate(ctx context.Context, doer *user_model.User, pd *packages_model.PackageDescriptor)
 	NotifyPackageDelete(ctx context.Context, doer *user_model.User, pd *packages_model.PackageDescriptor)

cmd/hook.go
@@ -201,7 +201,7 @@ Gitea or set your environment appropriately.`, "")
 
 	oldCommitIDs := make([]string, hookBatchSize)
 	newCommitIDs := make([]string, hookBatchSize)
-	refFullNames := make([]string, hookBatchSize)
+	refFullNames := make([]git.RefName, hookBatchSize)
 	count := 0
 	total := 0
 	lastline := 0
@@ -236,14 +236,14 @@ Gitea or set your environment appropriately.`, "")
 
 		oldCommitID := string(fields[0])
 		newCommitID := string(fields[1])
-		refFullName := string(fields[2])
+		refFullName := git.RefName(fields[2])
 		total++
 		lastline++
 
 		// If the ref is a branch or tag, check if it's protected
 		// if supportProcReceive all ref should be checked because
 		// permission check was delayed
-		if supportProcReceive || strings.HasPrefix(refFullName, git.BranchPrefix) || strings.HasPrefix(refFullName, git.TagPrefix) {
+		if supportProcReceive || refFullName.IsBranch() || refFullName.IsTag() {
 			oldCommitIDs[count] = oldCommitID
 			newCommitIDs[count] = newCommitID
 			refFullNames[count] = refFullName
@@ -351,7 +351,7 @@ Gitea or set your environment appropriately.`, "")
 	}
 	oldCommitIDs := make([]string, hookBatchSize)
 	newCommitIDs := make([]string, hookBatchSize)
-	refFullNames := make([]string, hookBatchSize)
+	refFullNames := make([]git.RefName, hookBatchSize)
 	count := 0
 	total := 0
 	wasEmpty := false
@@ -373,7 +373,7 @@ Gitea or set your environment appropriately.`, "")
 		fmt.Fprintf(out, ".")
 		oldCommitIDs[count] = string(fields[0])
 		newCommitIDs[count] = string(fields[1])
-		refFullNames[count] = string(fields[2])
+		refFullNames[count] = git.RefName(fields[2])
 		if refFullNames[count] == git.BranchPrefix+"master" && newCommitIDs[count] != git.EmptySHA && count == total {
 			masterPushed = true
 		}
@@ -575,7 +575,7 @@ Gitea or set your environment appropriately.`, "")
 	}
 	hookOptions.OldCommitIDs = make([]string, 0, hookBatchSize)
 	hookOptions.NewCommitIDs = make([]string, 0, hookBatchSize)
-	hookOptions.RefFullNames = make([]string, 0, hookBatchSize)
+	hookOptions.RefFullNames = make([]git.RefName, 0, hookBatchSize)
 
 	for {
 		// note: pktLineTypeUnknow means pktLineTypeFlush and pktLineTypeData all allowed
@@ -593,7 +593,7 @@ Gitea or set your environment appropriately.`, "")
 		}
 		hookOptions.OldCommitIDs = append(hookOptions.OldCommitIDs, t[0])
 		hookOptions.NewCommitIDs = append(hookOptions.NewCommitIDs, t[1])
-		hookOptions.RefFullNames = append(hookOptions.RefFullNames, t[2])
+		hookOptions.RefFullNames = append(hookOptions.RefFullNames, git.RefName(t[2]))
 	}
 
 	hookOptions.GitPushOptions = make(map[string]string)
@@ -640,7 +640,7 @@ Gitea or set your environment appropriately.`, "")
 
 	for _, rs := range resp.Results {
 		if len(rs.Err) > 0 {
-			err = writeDataPktLine(ctx, os.Stdout, []byte("ng "+rs.OriginalRef+" "+rs.Err))
+			err = writeDataPktLine(ctx, os.Stdout, []byte("ng "+rs.OriginalRef.String()+" "+rs.Err))
 			if err != nil {
 				return err
 			}
@@ -648,7 +648,7 @@ Gitea or set your environment appropriately.`, "")
 		}
 
 		if rs.IsNotMatched {
-			err = writeDataPktLine(ctx, os.Stdout, []byte("ok "+rs.OriginalRef))
+			err = writeDataPktLine(ctx, os.Stdout, []byte("ok "+rs.OriginalRef.String()))
 			if err != nil {
 				return err
 			}

models/actions/run.go
@@ -70,7 +70,7 @@ func (run *ActionRun) Link() string {
 // RefLink return the url of run's ref
 func (run *ActionRun) RefLink() string {
 	refName := git.RefName(run.Ref)
-	if refName.RefGroup() == "pull" {
+	if refName.IsPull() {
 		return run.Repo.Link() + "/pulls/" + refName.ShortName()
 	}
 	return git.RefURL(run.Repo.Link(), run.Ref)
@@ -79,7 +79,7 @@ func (run *ActionRun) RefLink() string {
 // PrettyRef return #id for pull ref or ShortName for others
 func (run *ActionRun) PrettyRef() string {
 	refName := git.RefName(run.Ref)
-	if refName.RefGroup() == "pull" {
+	if refName.IsPull() {
 		return "#" + strings.TrimSuffix(strings.TrimPrefix(run.Ref, git.PullPrefix), "/head")
 	}
 	return refName.ShortName()

commit 2c281289f0fd46cb5a11f307a7c7a4322c58d816

Fix compatible for webhook ref type (#25195)
Fix #25185 
Caused by #24634

modules/git/ref.go
@@ -163,6 +163,7 @@ func (ref RefName) ShortName() string {
 }
 
 // RefGroup returns the group type of the reference
+// Using the name of the directory under .git/refs
 func (ref RefName) RefGroup() string {
 	if ref.IsBranch() {
 		return "heads"
@@ -180,6 +181,19 @@ func (ref RefName) RefGroup() string {
 		return "for"
 	}
 	return ""
+}
+
+// RefType returns the simple ref type of the reference, e.g. branch, tag
+// It's differrent from RefGroup, which is using the name of the directory under .git/refs
+// Here we using branch but not heads, using tag but not tags
+func (ref RefName) RefType() string {
+	var refType string
+	if ref.IsBranch() {
+		refType = "branch"
+	} else if ref.IsTag() {
+		refType = "tag"
+	}
+	return refType
 }
 
 // RefURL returns the absolute URL for a ref in a repository

routers/api/actions/runner/utils.go
@@ -98,8 +98,8 @@ func generateTaskContext(t *actions_model.ActionTask) *structpb.Struct {
 		baseRef = pullPayload.PullRequest.Base.Ref
 		headRef = pullPayload.PullRequest.Head.Ref
 	}
+
 	refName := git.RefName(t.Job.Run.Ref)
-	refType := refName.RefGroup()
 
 	taskContext, err := structpb.NewStruct(map[string]interface{}{
 		// standard contexts, see https://docs.github.com/en/actions/learn-github-actions/contexts#github-context
@@ -121,7 +121,7 @@ func generateTaskContext(t *actions_model.ActionTask) *structpb.Struct {
 		"ref":               t.Job.Run.Ref,                                        // string, The fully-formed ref of the branch or tag that triggered the workflow run. For workflows triggered by push, this is the branch or tag ref that was pushed. For workflows triggered by pull_request, this is the pull request merge branch. For workflows triggered by release, this is the release tag created. For other triggers, this is the branch or tag ref that triggered the workflow run. This is only set if a branch or tag is available for the event type. The ref given is fully-formed, meaning that for branches the format is refs/heads/<branch_name>, for pull requests it is refs/pull/<pr_number>/merge, and for tags it is refs/tags/<tag_name>. For example, refs/heads/feature-branch-1.
 		"ref_name":          refName.String(),                                     // string, The short ref name of the branch or tag that triggered the workflow run. This value matches the branch or tag name shown on GitHub. For example, feature-branch-1.
 		"ref_protected":     false,                                                // boolean, true if branch protections are configured for the ref that triggered the workflow run.
-		"ref_type":          refType,                                              // string, The type of ref that triggered the workflow run. Valid values are branch or tag.
+		"ref_type":          refName.RefType(),                                    // string, The type of ref that triggered the workflow run. Valid values are branch or tag.
 		"path":              "",                                                   // string, Path on the runner to the file that sets system PATH variables from workflow commands. This file is unique to the current step and is a different file for each step in a job. For more information, see "Workflow commands for GitHub Actions."
 		"repository":        t.Job.Run.Repo.OwnerName + "/" + t.Job.Run.Repo.Name, // string, The owner and repository name. For example, Codertocat/Hello-World.
 		"repository_owner":  t.Job.Run.Repo.OwnerName,                             // string, The repository owner's name. For example, Codertocat.

services/actions/notifier.go
@@ -384,7 +384,7 @@ func (n *actionsNotifier) NotifyCreateRef(ctx context.Context, pusher *user_mode
 		WithPayload(&api.CreatePayload{
 			Ref:     refFullName.ShortName(),
 			Sha:     refID,
-			RefType: refFullName.RefGroup(),
+			RefType: refFullName.RefType(),
 			Repo:    apiRepo,
 			Sender:  apiPusher,
 		}).
@@ -401,7 +401,7 @@ func (n *actionsNotifier) NotifyDeleteRef(ctx context.Context, pusher *user_mode
 		WithRef(refFullName.ShortName()). // FIXME: should we use a full ref name
 		WithPayload(&api.DeletePayload{
 			Ref:        refFullName.ShortName(),
-			RefType:    refFullName.RefGroup(),
+			RefType:    refFullName.RefType(),
 			PusherType: api.PusherTypeUser,
 			Repo:       apiRepo,
 			Sender:     apiPusher,

services/webhook/notifier.go
@@ -755,7 +755,7 @@ func (m *webhookNotifier) NotifyCreateRef(ctx context.Context, pusher *user_mode
 	if err := PrepareWebhooks(ctx, EventSource{Repository: repo}, webhook_module.HookEventCreate, &api.CreatePayload{
 		Ref:     refName, // FIXME: should it be a full ref name?
 		Sha:     refID,
-		RefType: refFullName.RefGroup(),
+		RefType: refFullName.RefType(),
 		Repo:    apiRepo,
 		Sender:  apiPusher,
 	}); err != nil {
@@ -791,12 +791,12 @@ func (m *webhookNotifier) NotifyDeleteRef(ctx context.Context, pusher *user_mode
 
 	if err := PrepareWebhooks(ctx, EventSource{Repository: repo}, webhook_module.HookEventDelete, &api.DeletePayload{
 		Ref:        refName, // FIXME: should it be a full ref name?
-		RefType:    refFullName.RefGroup(),
+		RefType:    refFullName.RefType(),
 		PusherType: api.PusherTypeUser,
 		Repo:       apiRepo,
 		Sender:     apiPusher,
 	}); err != nil {
-		log.Error("PrepareWebhooks.(delete %s): %v", refFullName.RefGroup(), err)
+		log.Error("PrepareWebhooks.(delete %s): %v", refFullName.RefType(), err)
 	}
 }
 