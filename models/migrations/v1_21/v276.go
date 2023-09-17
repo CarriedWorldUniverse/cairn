CarriedWorldUniverse/cairn

commit 3db0f5490adf61694a2ce74200aba4aa2c8dbb53

Add `RemoteAddress` to mirrors (#26952)
This PR adds a new field `RemoteAddress` to both mirror types which
contains the sanitized remote address for easier (database) access to
that information. Will be used in the audit PR if merged.

models/migrations/v1_21/v276.go
@@ -1,0 +1,132 @@
+// Copyright 2023 The Gitea Authors. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+package v1_21 //nolint
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"code.gitea.io/gitea/modules/git"
+	giturl "code.gitea.io/gitea/modules/git/url"
+	"code.gitea.io/gitea/modules/setting"
+
+	"xorm.io/xorm"
+)
+
+func AddRemoteAddressToMirrors(x *xorm.Engine) error {
+	type Mirror struct {
+		RemoteAddress string `xorm:"VARCHAR(2048)"`
+	}
+
+	type PushMirror struct {
+		RemoteAddress string `xorm:"VARCHAR(2048)"`
+	}
+
+	if err := x.Sync(new(Mirror), new(PushMirror)); err != nil {
+		return err
+	}
+
+	if err := migratePullMirrors(x); err != nil {
+		return err
+	}
+
+	return migratePushMirrors(x)
+}
+
+func migratePullMirrors(x *xorm.Engine) error {
+	type Mirror struct {
+		ID            int64  `xorm:"pk autoincr"`
+		RepoID        int64  `xorm:"INDEX"`
+		RemoteAddress string `xorm:"VARCHAR(2048)"`
+	}
+
+	sess := x.NewSession()
+	defer sess.Close()
+
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+
+	if err := sess.Iterate(new(Mirror), func(_ int, bean any) error {
+		m := bean.(*Mirror)
+		remoteAddress, err := getRemoteAddress(sess, m.RepoID, "origin")
+		if err != nil {
+			return err
+		}
+
+		m.RemoteAddress = remoteAddress
+
+		_, err = sess.ID(m.ID).Cols("remote_address").Update(m)
+		return err
+	}); err != nil {
+		return err
+	}
+
+	return sess.Commit()
+}
+
+func migratePushMirrors(x *xorm.Engine) error {
+	type PushMirror struct {
+		ID            int64 `xorm:"pk autoincr"`
+		RepoID        int64 `xorm:"INDEX"`
+		RemoteName    string
+		RemoteAddress string `xorm:"VARCHAR(2048)"`
+	}
+
+	sess := x.NewSession()
+	defer sess.Close()
+
+	if err := sess.Begin(); err != nil {
+		return err
+	}
+
+	if err := sess.Iterate(new(PushMirror), func(_ int, bean any) error {
+		m := bean.(*PushMirror)
+		remoteAddress, err := getRemoteAddress(sess, m.RepoID, m.RemoteName)
+		if err != nil {
+			return err
+		}
+
+		m.RemoteAddress = remoteAddress
+
+		_, err = sess.ID(m.ID).Cols("remote_address").Update(m)
+		return err
+	}); err != nil {
+		return err
+	}
+
+	return sess.Commit()
+}
+
+func getRemoteAddress(sess *xorm.Session, repoID int64, remoteName string) (string, error) {
+	var ownerName string
+	var repoName string
+	has, err := sess.
+		Table("repository").
+		Cols("owner_name", "lower_name").
+		Where("id=?", repoID).
+		Get(&ownerName, &repoName)
+	if err != nil {
+		return "", err
+	} else if !has {
+		return "", fmt.Errorf("repository [%v] not found", repoID)
+	}
+
+	repoPath := filepath.Join(setting.RepoRootPath, strings.ToLower(ownerName), strings.ToLower(repoName)+".git")
+
+	remoteURL, err := git.GetRemoteAddress(context.Background(), repoPath, remoteName)
+	if err != nil {
+		return "", err
+	}
+
+	u, err := giturl.Parse(remoteURL)
+	if err != nil {
+		return "", err
+	}
+	u.User = nil
+
+	return u.String(), nil
+}

models/migrations/migrations.go
@@ -532,6 +532,8 @@ var migrations = []Migration{
 	NewMigration("Add Actions artifacts expiration date", v1_21.AddExpiredUnixColumnInActionArtifactTable),
 	// v275 -> v276
 	NewMigration("Add ScheduleID for ActionRun", v1_21.AddScheduleIDForActionRun),
+	// v276 -> v277
+	NewMigration("Add RemoteAddress to mirrors", v1_21.AddRemoteAddressToMirrors),
 }
 
 // GetCurrentDBVersion returns the current db version

models/repo/mirror.go
@@ -31,7 +31,7 @@ type Mirror struct {
 	LFS         bool   `xorm:"lfs_enabled NOT NULL DEFAULT false"`
 	LFSEndpoint string `xorm:"lfs_endpoint TEXT"`
 
-	Address string `xorm:"-"`
+	RemoteAddress string `xorm:"VARCHAR(2048)"`
 }
 
 func init() {

models/repo/pushmirror.go
@@ -20,10 +20,11 @@ var ErrPushMirrorNotExist = util.NewNotExistErrorf("PushMirror does not exist")
 
 // PushMirror represents mirror information of a repository.
 type PushMirror struct {
-	ID         int64       `xorm:"pk autoincr"`
-	RepoID     int64       `xorm:"INDEX"`
-	Repo       *Repository `xorm:"-"`
-	RemoteName string
+	ID            int64       `xorm:"pk autoincr"`
+	RepoID        int64       `xorm:"INDEX"`
+	Repo          *Repository `xorm:"-"`
+	RemoteName    string
+	RemoteAddress string `xorm:"VARCHAR(2048)"`
 
 	SyncOnCommit   bool `xorm:"NOT NULL DEFAULT true"`
 	Interval       time.Duration
@@ -31,6 +32,7 @@ type PushMirror struct {
 	LastUpdateUnix timeutil.TimeStamp `xorm:"INDEX last_update"`
 	LastError      string             `xorm:"text"`
 }
+
 type PushMirrorOptions struct {
 	ID         int64
 	RepoID     int64

models/repo/repo.go
@@ -191,12 +191,8 @@ func (repo *Repository) SanitizedOriginalURL() string {
 	if repo.OriginalURL == "" {
 		return ""
 	}
-	u, err := url.Parse(repo.OriginalURL)
-	if err != nil {
-		return ""
-	}
-	u.User = nil
-	return u.String()
+	u, _ := util.SanitizeURL(repo.OriginalURL)
+	return u
 }
 
 // text representations to be returned in SizeDetail.Name

modules/repository/repo.go
@@ -180,12 +180,17 @@ func MigrateRepositoryGitData(ctx context.Context, u *user_model.User,
 	defer committer.Close()
 
 	if opts.Mirror {
+		remoteAddress, err := util.SanitizeURL(opts.CloneAddr)
+		if err != nil {
+			return repo, err
+		}
 		mirrorModel := repo_model.Mirror{
 			RepoID:         repo.ID,
 			Interval:       setting.Mirror.DefaultInterval,
 			EnablePrune:    true,
 			NextUpdateUnix: timeutil.TimeStampNow().AddDuration(setting.Mirror.DefaultInterval),
 			LFS:            opts.LFS,
+			RemoteAddress:  remoteAddress,
 		}
 		if opts.LFS {
 			mirrorModel.LFSEndpoint = opts.LFSEndpoint

modules/util/url.go
@@ -39,3 +39,12 @@ func URLJoin(base string, elems ...string) string {
 	}
 	return joinedURL
 }
+
+func SanitizeURL(s string) (string, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return "", err
+	}
+	u.User = nil
+	return u.String(), nil
+}

routers/api/v1/repo/mirror.go
@@ -353,12 +353,19 @@ func CreatePushMirror(ctx *context.APIContext, mirrorOption *api.CreatePushMirro
 		return
 	}
 
+	remoteAddress, err := util.SanitizeURL(mirrorOption.RemoteAddress)
+	if err != nil {
+		ctx.ServerError("SanitizeURL", err)
+		return
+	}
+
 	pushMirror := &repo_model.PushMirror{
-		RepoID:       repo.ID,
-		Repo:         repo,
-		RemoteName:   fmt.Sprintf("remote_mirror_%s", remoteSuffix),
-		Interval:     interval,
-		SyncOnCommit: mirrorOption.SyncOnCommit,
+		RepoID:        repo.ID,
+		Repo:          repo,
+		RemoteName:    fmt.Sprintf("remote_mirror_%s", remoteSuffix),
+		Interval:      interval,
+		SyncOnCommit:  mirrorOption.SyncOnCommit,
+		RemoteAddress: remoteAddress,
 	}
 
 	if err = repo_model.InsertPushMirror(ctx, pushMirror); err != nil {

commit 2c8f01f1477fde7ddecc20e60eaf973d9bf7ca3a

Fix wrong migration for email address (#27106)
On Iterate, `sess` should not be used in the closure function body.

Caused by #26952

models/migrations/v1_21/v276.go
@@ -50,19 +50,45 @@ func migratePullMirrors(x *xorm.Engine) error {
 		return err
 	}
 
-	if err := sess.Iterate(new(Mirror), func(_ int, bean any) error {
-		m := bean.(*Mirror)
-		remoteAddress, err := getRemoteAddress(sess, m.RepoID, "origin")
-		if err != nil {
+	limit := setting.Database.IterateBufferSize
+	if limit <= 0 {
+		limit = 50
+	}
+
+	start := 0
+
+	for {
+		var mirrors []Mirror
+		if err := sess.Limit(limit, start).Find(&mirrors); err != nil {
 			return err
 		}
 
-		m.RemoteAddress = remoteAddress
+		if len(mirrors) == 0 {
+			break
+		}
+		start += len(mirrors)
 
-		_, err = sess.ID(m.ID).Cols("remote_address").Update(m)
-		return err
-	}); err != nil {
-		return err
+		for _, m := range mirrors {
+			remoteAddress, err := getRemoteAddress(sess, m.RepoID, "origin")
+			if err != nil {
+				return err
+			}
+
+			m.RemoteAddress = remoteAddress
+
+			if _, err = sess.ID(m.ID).Cols("remote_address").Update(m); err != nil {
+				return err
+			}
+		}
+
+		if start%1000 == 0 { // avoid a too big transaction
+			if err := sess.Commit(); err != nil {
+				return err
+			}
+			if err := sess.Begin(); err != nil {
+				return err
+			}
+		}
 	}
 
 	return sess.Commit()
@@ -83,19 +109,45 @@ func migratePushMirrors(x *xorm.Engine) error {
 		return err
 	}
 
-	if err := sess.Iterate(new(PushMirror), func(_ int, bean any) error {
-		m := bean.(*PushMirror)
-		remoteAddress, err := getRemoteAddress(sess, m.RepoID, m.RemoteName)
-		if err != nil {
+	limit := setting.Database.IterateBufferSize
+	if limit <= 0 {
+		limit = 50
+	}
+
+	start := 0
+
+	for {
+		var mirrors []PushMirror
+		if err := sess.Limit(limit, start).Find(&mirrors); err != nil {
 			return err
 		}
 
-		m.RemoteAddress = remoteAddress
+		if len(mirrors) == 0 {
+			break
+		}
+		start += len(mirrors)
 
-		_, err = sess.ID(m.ID).Cols("remote_address").Update(m)
-		return err
-	}); err != nil {
-		return err
+		for _, m := range mirrors {
+			remoteAddress, err := getRemoteAddress(sess, m.RepoID, m.RemoteName)
+			if err != nil {
+				return err
+			}
+
+			m.RemoteAddress = remoteAddress
+
+			if _, err = sess.ID(m.ID).Cols("remote_address").Update(m); err != nil {
+				return err
+			}
+		}
+
+		if start%1000 == 0 { // avoid a too big transaction
+			if err := sess.Commit(); err != nil {
+				return err
+			}
+			if err := sess.Begin(); err != nil {
+				return err
+			}
+		}
 	}
 
 	return sess.Commit()