yonasBSD/forgejo

[GITEA] Detect file rename and show in history
- Add a indication to the file history if the file has been renamed,
this indication contains a link to browse the history of the file
further.
- Added unit testing.
- Added integration testing.
- Resolves https://codeberg.org/forgejo/forgejo/issues/1279

tests/integration/repo_test.go
@@ -686,3 +686,33 @@ func TestDangerZoneConfirmation(t *testing.T) {
 		})
 	})
 }
+
+func TestReanmedFileHistory(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	t.Run("Renamed file", func(t *testing.T) {
+		defer tests.PrintCurrentTest(t)()
+
+		req := NewRequest(t, "GET", "/user2/repo59/commits/branch/master/license")
+		resp := MakeRequest(t, req, http.StatusOK)
+
+		htmlDoc := NewHTMLParser(t, resp.Body)
+
+		renameNotice := htmlDoc.doc.Find(".ui.bottom.attached.header")
+		assert.Equal(t, 1, renameNotice.Length())
+		assert.Contains(t, renameNotice.Text(), "Renamed from licnse (Browse further)")
+
+		oldFileHistoryLink, ok := renameNotice.Find("a").Attr("href")
+		assert.True(t, ok)
+		assert.Equal(t, "/user2/repo59/commits/commit/80b83c5c8220c3aa3906e081f202a2a7563ec879/licnse", oldFileHistoryLink)
+	})
+
+	t.Run("Non renamed file", func(t *testing.T) {
+		req := NewRequest(t, "GET", "/user2/repo59/commits/branch/master/README.md")
+		resp := MakeRequest(t, req, http.StatusOK)
+
+		htmlDoc := NewHTMLParser(t, resp.Body)
+
+		htmlDoc.AssertElement(t, ".ui.bottom.attached.header", false)
+	})
+}

CarriedWorldUniverse/cairn

(cherry picked from commit 8935e3433194053595abbc2be5c2e8b635414593)
(cherry picked from commit a876d7678ffd80c4eac0de1c4b099490b070838e)
Conflicts:
	options/locale/locale_en-US.ini
	https://codeberg.org/forgejo/forgejo/pulls/1550
(cherry picked from commit 3a5af3a613d8a51731037b3457b8ac0b5bd23d16)
(cherry picked from commit bf3cd314606fb401158a5dad976253a26ed92487)
(cherry picked from commit 332a9999b717b2bae02310d488e13c52d3e177fa)
(cherry picked from commit 9fe227648a6531eca4911daba8781742f4232599)
(cherry picked from commit c2612ba64361d9f17ed78bf69428e8addf728086)
(cherry picked from commit e192b1dbc8ea407ddec2bb19008a847c493ae3b8)
[GITEA] Detect file rename and show in history (squash) ctx.Locale
(cherry picked from commit 070c7fa6e10d09fc4a4911bdf89dea60965815d0)
(cherry picked from commit 5d73ca2113a6a1124acdab54322c89b2eaa01834)
(cherry picked from commit 7847ceaf2dc32eea9717401a9be110cf7956ca6a)
(cherry picked from commit b7cf3ec83c701be958a4c170e361b59896418823)
(cherry picked from commit 806d8b928952274afc7ed1cc358167cc54b22b86)
(cherry picked from commit 4836647e6d4c1ec409ed83013479dc04c43e9a21)
(cherry picked from commit d05b6ab727062287e3bb8abf94ec38b6656495e0)
(cherry picked from commit 975b075f3eb60bd9ee0d168bd5dea4739aa55180)
	tests/integration/repo_test.go
	https://codeberg.org/forgejo/forgejo/pulls/2119
(cherry picked from commit 24dedd4cd88e2f42c92f9a1e8bcdfdf09737a436)
(cherry picked from commit 116d46549a82e8c76668b29e34d39b35c161b8ca)
(cherry picked from commit 328e81b99408bf6d005d1421d6cc6de9d889165b)
(cherry picked from commit c65d05992cf8099902b025e5edd7fa717e0d5ff8)

tests/integration/repo_test.go
@@ -473,6 +473,36 @@ func TestViewRepoDirectoryReadme(t *testing.T) {
 	missing("symlink-loop", "/user2/readme-test/src/branch/symlink-loop/")
 }
 
+func TestRenamedFileHistory(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	t.Run("Renamed file", func(t *testing.T) {
+		defer tests.PrintCurrentTest(t)()
+
+		req := NewRequest(t, "GET", "/user2/repo59/commits/branch/master/license")
+		resp := MakeRequest(t, req, http.StatusOK)
+
+		htmlDoc := NewHTMLParser(t, resp.Body)
+
+		renameNotice := htmlDoc.doc.Find(".ui.bottom.attached.header")
+		assert.Equal(t, 1, renameNotice.Length())
+		assert.Contains(t, renameNotice.Text(), "Renamed from licnse (Browse further)")
+
+		oldFileHistoryLink, ok := renameNotice.Find("a").Attr("href")
+		assert.True(t, ok)
+		assert.Equal(t, "/user2/repo59/commits/commit/80b83c5c8220c3aa3906e081f202a2a7563ec879/licnse", oldFileHistoryLink)
+	})
+
+	t.Run("Non renamed file", func(t *testing.T) {
+		req := NewRequest(t, "GET", "/user2/repo59/commits/branch/master/README.md")
+		resp := MakeRequest(t, req, http.StatusOK)
+
+		htmlDoc := NewHTMLParser(t, resp.Body)
+
+		htmlDoc.AssertElement(t, ".ui.bottom.attached.header", false)
+	})
+}
+
 func TestMarkDownReadmeImage(t *testing.T) {
 	defer tests.PrepareTestEnv(t)()
 