CarriedWorldUniverse/cairn

commit 78172fd36eccb44aca8c1e113355673883ded742

show user who created the repository instead of the organization in action feed (#5948)

models/repo.go
@@ -1359,14 +1359,14 @@ func createRepository(e *xorm.Session, doer, u *User, repo *Repository) (err err
 			return fmt.Errorf("watchRepo: %v", err)
 		}
 	}
-	if err = newRepoAction(e, u, repo); err != nil {
+	if err = newRepoAction(e, doer, repo); err != nil {
 		return fmt.Errorf("newRepoAction: %v", err)
 	}
 
 	return nil
 }
 
-// CreateRepository creates a repository for the user/organization u.
+// CreateRepository creates a repository for the user/organization.
 func CreateRepository(doer, u *User, opts CreateRepoOptions) (_ *Repository, err error) {
 	if !doer.IsAdmin && !u.CanCreateRepo() {
 		return nil, ErrReachLimitOfRepo{u.MaxRepoCreation}

commit 0eb56ac78d1e025f4b6224d535a8d8ae33b8e38b

chore: fix e2e
- Regression from #5948
- Use proper permission.
- Remove debug statement

tests/e2e/utils_e2e_test.go
@@ -8,6 +8,8 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"os"
+	"regexp"
 	"testing"
 	"time"
 
@@ -16,6 +18,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 )
+
+var rootPathRe = regexp.MustCompile("\\[repository\\]\nROOT\\s=\\s.*")
 
 func onForgejoRunTB(t testing.TB, callback func(testing.TB, *url.URL), prepare ...bool) {
 	if len(prepare) == 0 || prepare[0] {
@@ -37,7 +41,13 @@ func onForgejoRunTB(t testing.TB, callback func(testing.TB, *url.URL), prepare .
 	require.NoError(t, err)
 	u.Host = listener.Addr().String()
 
+	// Override repository root in config.
+	conf, err := os.ReadFile(setting.CustomConf)
+	require.NoError(t, err)
+	require.NoError(t, os.WriteFile(setting.CustomConf, rootPathRe.ReplaceAll(conf, []byte("[repository]\nROOT = "+setting.RepoRootPath)), 0o644))
+
 	defer func() {
+		require.NoError(t, os.WriteFile(setting.CustomConf, conf, 0o644))
 		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 		s.Shutdown(ctx)
 		cancel()

tests/integration/git_helper_for_declarative_test.go
@@ -83,10 +83,10 @@ func onGiteaRun[T testing.TB](t T, callback func(T, *url.URL)) {
 	// Override repository root in config.
 	conf, err := os.ReadFile(setting.CustomConf)
 	require.NoError(t, err)
-	require.NoError(t, os.WriteFile(setting.CustomConf, rootPathRe.ReplaceAll(conf, []byte("[repository]\nROOT = "+setting.RepoRootPath)), os.ModePerm))
+	require.NoError(t, os.WriteFile(setting.CustomConf, rootPathRe.ReplaceAll(conf, []byte("[repository]\nROOT = "+setting.RepoRootPath)), 0o600))
 
 	defer func() {
-		require.NoError(t, os.WriteFile(setting.CustomConf, conf, os.ModePerm))
+		require.NoError(t, os.WriteFile(setting.CustomConf, conf, 0o600))
 		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 		s.Shutdown(ctx)
 		cancel()

tests/test_utils.go
@@ -201,7 +201,6 @@ func InitTest(requireGitea bool) {
 	if err != nil {
 		log.Fatal("os.ReadDir: %v", err)
 	}
-	fmt.Println(ownerDirs)
 
 	for _, ownerDir := range ownerDirs {
 		if !ownerDir.Type().IsDir() {