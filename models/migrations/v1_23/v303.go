CarriedWorldUniverse/cairn

commit 76b1098340fa47e68d9d02e0e4c21ec16ccc1ac2

fix: Gitea last drop
Some database fields were added in the database to facilitate
migration from Gitea and do not serve any purpose. Drop them since
v1.22 is the last version of the database to allow a transparent
migration to Forgejo.

models/migrations/v1_23/v303.go
@@ -1,0 +1,33 @@
+// Copyright 2024 The Forgejo Authors.
+// SPDX-License-Identifier: MIT
+
+package v1_23 //nolint
+
+import (
+	"code.gitea.io/gitea/models/migrations/base"
+
+	"xorm.io/xorm"
+)
+
+func GiteaLastDrop(x *xorm.Engine) error {
+	sess := x.NewSession()
+	defer sess.Close()
+
+	if err := base.DropTableColumns(sess, "badge", "slug"); err != nil {
+		return err
+	}
+	if err := base.DropTableColumns(sess, "oauth2_application", "skip_secondary_authorization"); err != nil {
+		return err
+	}
+	if err := base.DropTableColumns(sess, "repository", "default_wiki_branch"); err != nil {
+		return err
+	}
+	// the migration v297.go that adds everyone_access_mode exists in Gitea >= v1.22 and the column must be dropped
+	// but it does not exist in Forgejo and a failure to drop the column can be ignored
+	base.DropTableColumns(sess, "repo_unit", "everyone_access_mode")
+	if err := base.DropTableColumns(sess, "protected_branch", "can_force_push", "enable_force_push_allowlist", "force_push_allowlist_user_i_ds", "force_push_allowlist_team_i_ds", "force_push_allowlist_deploy_keys"); err != nil {
+		return err
+	}
+
+	return sess.Commit()
+}

models/migrations/migrations.go
@@ -362,6 +362,9 @@ func prepareMigrationTasks() []*migration {
 		newMigration(300, "Add force-push branch protection support", v1_23.AddForcePushBranchProtection),
 		newMigration(301, "Add skip_secondary_authorization option to oauth2 application table", v1_23.AddSkipSecondaryAuthColumnToOAuth2ApplicationTable),
 		newMigration(302, "Add index to action_task stopped log_expired", v1_23.AddIndexToActionTaskStoppedLogExpired),
+
+		// Migration to Forgejo v10
+		newMigration(303, "Gitea last drop", v1_23.GiteaLastDrop),
 	}
 	return preparedMigrations
 }

commit 44981f50447f74e3e16eba5149b2840307ff72ab

fix: avoid Gitea migration warnings (take 2) (#6577)
76b1098340fa47e68d9d02e0e4c21ec16ccc1ac2 introduced a regression in https://codeberg.org/forgejo/forgejo/pulls/6343

Trying to remove fields that have already been removed by

https://codeberg.org/forgejo/forgejo/src/commit/4f951f140efaa364e04577f33858e5df3cad0082/models/forgejo_migrations/v14.go

Is a noop for SQLite and went undetected by the upgrade tests.

Fixes: https://codeberg.org/forgejo/forgejo/issues/6575
Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/6577
Reviewed-by: Gusted <gusted@noreply.codeberg.org>
Reviewed-by: Michael Kriese <michael.kriese@gmx.de>
Co-authored-by: Earl Warren <contact@earl-warren.org>
Co-committed-by: Earl Warren <contact@earl-warren.org>

models/migrations/v1_23/v303.go
@@ -4,6 +4,8 @@
 package v1_23 //nolint
 
 import (
+	"fmt"
+
 	"code.gitea.io/gitea/models/migrations/base"
 
 	"xorm.io/xorm"
@@ -13,20 +15,26 @@ func GiteaLastDrop(x *xorm.Engine) error {
 	sess := x.NewSession()
 	defer sess.Close()
 
-	if err := base.DropTableColumns(sess, "badge", "slug"); err != nil {
-		return err
-	}
-	if err := base.DropTableColumns(sess, "oauth2_application", "skip_secondary_authorization"); err != nil {
-		return err
-	}
-	if err := base.DropTableColumns(sess, "repository", "default_wiki_branch"); err != nil {
-		return err
-	}
-	// the migration v297.go that adds everyone_access_mode exists in Gitea >= v1.22 and the column must be dropped
-	// but it does not exist in Forgejo and a failure to drop the column can be ignored
-	base.DropTableColumns(sess, "repo_unit", "everyone_access_mode")
-	if err := base.DropTableColumns(sess, "protected_branch", "can_force_push", "enable_force_push_allowlist", "force_push_allowlist_user_i_ds", "force_push_allowlist_team_i_ds", "force_push_allowlist_deploy_keys"); err != nil {
-		return err
+	for _, drop := range []struct {
+		table string
+		field string
+	}{
+		{"badge", "slug"},
+		{"oauth2_application", "skip_secondary_authorization"},
+		{"repository", "default_wiki_branch"},
+		{"repo_unit", "everyone_access_mode"},
+		{"protected_branch", "can_force_push"},
+		{"protected_branch", "enable_force_push_allowlist"},
+		{"protected_branch", "force_push_allowlist_user_i_ds"},
+		{"protected_branch", "force_push_allowlist_team_i_ds"},
+		{"protected_branch", "force_push_allowlist_deploy_keys"},
+	} {
+		if _, err := sess.Exec(fmt.Sprintf("SELECT `%s` FROM `%s` WHERE 0 = 1", drop.field, drop.table)); err != nil {
+			continue
+		}
+		if err := base.DropTableColumns(sess, drop.table, drop.field); err != nil {
+			return err
+		}
 	}
 
 	return sess.Commit()