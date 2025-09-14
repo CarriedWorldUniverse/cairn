CarriedWorldUniverse/cairn

commit 18b64983a496e1a4eba30f72c278029055d9637a

feat: ability to view previous logs for Actions runs that have been retried (#9017)
Adds a new dropdown to the job logs, visible only when a job has been retried at least once:
![action-with-dropdown](/attachments/9669b47e-2239-4f07-b823-2759dd99a4fb)

When an older run attempt from the dropdown is selected, displays the older run's logs:
![historical-action-logs](/attachments/8b737386-63fb-4f3f-b5b5-ac38c62ed648)

Context on implementation & design decisions:
- It is important that when a URL from an Action's log is shared, the person on the other side sees the exact same logs that were being viewed.  For this reason, all log views are automatically redirected to a fully-qualified URL (including the *run*, *job*, and *attempt*), so that when they are shared there is a guarantee of stability in the viewed logs.
- Individual jobs can be rerun any number of times independent of other jobs in the same workflow.  This means there isn't a "set" of related jobs that were executed at the same time, and this led me to remove the display of current status of jobs on the left-hand side of the view.  There isn't a logical set of job statuses to display here.

Fixes #1043.  Based upon @gmem's original work in #1416.

## Checklist

The [contributor guide](https://forgejo.org/docs/next/contributor/) contains information that will be helpful to first time contributors. There also are a few [conditions for merging Pull Requests in Forgejo repositories](https://codeberg.org/forgejo/governance/src/branch/main/PullRequestsAgreement.md). You are also welcome to join the [Forgejo development chatroom](https://matrix.to/#/#forgejo-development:matrix.org).

### Tests

- I added test coverage for Go changes...
  - [x] in their respective `*_test.go` for unit tests.
  - [ ] in the `tests/integration` directory if it involves interactions with a live Forgejo server.
- I added test coverage for JavaScript changes...
  - [x] in `web_src/js/*.test.js` if it can be unit tested.

models/actions/run_job.go
@@ -44,6 +44,31 @@ func init() {
 	db.RegisterModel(new(ActionRunJob))
 }
 
+func (job *ActionRunJob) HTMLURL(ctx context.Context) (string, error) {
+	if job.Run == nil || job.Run.Repo == nil {
+		return "", fmt.Errorf("action_run_job: load run and repo before accessing HTMLURL")
+	}
+
+	// Find the "index" of the currently selected job... kinda ugly that the URL uses the index rather than some other
+	// unique identifier of the job which could actually be stored upon it.  But hard to change that now.
+	allJobs, err := GetRunJobsByRunID(ctx, job.RunID)
+	if err != nil {
+		return "", err
+	}
+	jobIndex := -1
+	for i, otherJob := range allJobs {
+		if job.ID == otherJob.ID {
+			jobIndex = i
+			break
+		}
+	}
+	if jobIndex == -1 {
+		return "", fmt.Errorf("action_run_job: unable to find job on run: %d", job.ID)
+	}
+
+	return fmt.Sprintf("%s/actions/runs/%d/jobs/%d/attempt/%d", job.Run.Repo.HTMLURL(), job.Run.Index, jobIndex, job.Attempt), nil
+}
+
 func (job *ActionRunJob) Duration() time.Duration {
 	return calculateDuration(job.Started, job.Stopped, job.Status)
 }

models/fixtures/action_run_job.yml
@@ -106,7 +106,7 @@
   commit_sha: 985f0301dba5e7b34be866819cd15ad3d8f508ee
   is_fork_pull_request: 0
   name: job_2
-  attempt: 1
+  attempt: 2
   job_id: job_2
   task_id: 47
   status: 5

routers/web/repo/actions/view_test.go
@@ -141,7 +141,7 @@ func Test_artifactsFindByNameOrID(t *testing.T) {
 	}
 }
 
-func baseExpectedResponse() *ViewResponse {
+func baseExpectedViewResponse() *ViewResponse {
 	return &ViewResponse{
 		State: ViewState{
 			Run: ViewRunInfo{
@@ -193,6 +193,23 @@ func baseExpectedResponse() *ViewResponse {
 						Status:  "waiting",
 					},
 				},
+				AllAttempts: []*TaskAttempt{
+					{
+						Number:  3,
+						Started: template.HTML("<relative-time prefix=\"\" tense=\"past\" datetime=\"2023-05-09T12:48:48Z\" data-tooltip-content data-tooltip-interactive=\"true\">2023-05-09 12:48:48 +00:00</relative-time>"),
+						Status:  "running",
+					},
+					{
+						Number:  2,
+						Started: template.HTML("<relative-time prefix=\"\" tense=\"past\" datetime=\"2023-05-09T12:48:48Z\" data-tooltip-content data-tooltip-interactive=\"true\">2023-05-09 12:48:48 +00:00</relative-time>"),
+						Status:  "success",
+					},
+					{
+						Number:  1,
+						Started: template.HTML("<relative-time prefix=\"\" tense=\"past\" datetime=\"2023-05-09T12:48:48Z\" data-tooltip-content data-tooltip-interactive=\"true\">2023-05-09 12:48:48 +00:00</relative-time>"),
+						Status:  "success",
+					},
+				},
 			},
 		},
 		Logs: ViewLogs{
@@ -208,22 +225,27 @@ func TestActionsViewViewPost(t *testing.T) {
 		name           string
 		runIndex       int64
 		jobIndex       int64
+		attemptNumber  int64
 		expected       *ViewResponse
 		expectedTweaks func(*ViewResponse)
 	}{
 		{
-			name:     "base case",
-			runIndex: 187,
-			jobIndex: 0,
-			expected: baseExpectedResponse(),
+			name:          "base case",
+			runIndex:      187,
+			jobIndex:      0,
+			attemptNumber: 1,
+			expected:      baseExpectedViewResponse(),
 			expectedTweaks: func(resp *ViewResponse) {
+				resp.State.CurrentJob.Steps[0].Status = "success"
+				resp.State.CurrentJob.Steps[1].Status = "success"
 			},
 		},
 		{
-			name:     "run with waiting jobs",
-			runIndex: 189,
-			jobIndex: 0,
-			expected: baseExpectedResponse(),
+			name:          "run with waiting jobs",
+			runIndex:      189,
+			jobIndex:      0,
+			attemptNumber: 1,
+			expected:      baseExpectedViewResponse(),
 			expectedTweaks: func(resp *ViewResponse) {
 				// Variations from runIndex 187 -> runIndex 189 that are not the subject of this test...
 				resp.State.Run.Link = "/user5/repo4/actions/runs/189"
@@ -257,11 +279,29 @@ func TestActionsViewViewPost(t *testing.T) {
 						Status:  "success",
 					},
 				}
+				resp.State.CurrentJob.AllAttempts = []*TaskAttempt{
+					{
+						Number:  1,
+						Started: template.HTML("<relative-time prefix=\"\" tense=\"past\" datetime=\"2023-05-09T12:48:48Z\" data-tooltip-content data-tooltip-interactive=\"true\">2023-05-09 12:48:48 +00:00</relative-time>"),
+						Status:  "success",
+					},
+				}
 
 				// Under test in this case: verify that Done is set to false; in the fixture data, job.ID=195 is status
 				// Success, but job.ID=196 is status Waiting, and so we expect to signal Done=false to indicate to the
 				// UI to continue refreshing the page.
 				resp.State.Run.Done = false
+			},
+		},
+		{
+			name:          "attempt 3",
+			runIndex:      187,
+			jobIndex:      0,
+			attemptNumber: 3,
+			expected:      baseExpectedViewResponse(),
+			expectedTweaks: func(resp *ViewResponse) {
+				resp.State.CurrentJob.Steps[0].Status = "running"
+				resp.State.CurrentJob.Steps[1].Status = "waiting"
 			},
 		},
 	}
@@ -273,10 +313,11 @@ func TestActionsViewViewPost(t *testing.T) {
 			contexttest.LoadRepo(t, ctx, 4)
 			ctx.SetParams(":run", fmt.Sprintf("%d", tt.runIndex))
 			ctx.SetParams(":job", fmt.Sprintf("%d", tt.jobIndex))
+			ctx.SetParams(":attempt", fmt.Sprintf("%d", tt.attemptNumber))
 			web.SetForm(ctx, &ViewRequest{})
 
 			ViewPost(ctx)
-			require.Equal(t, http.StatusOK, resp.Result().StatusCode)
+			require.Equal(t, http.StatusOK, resp.Result().StatusCode, "failure in ViewPost(): %q", resp.Body.String())
 
 			var actual ViewResponse
 			err := json.Unmarshal(resp.Body.Bytes(), &actual)
@@ -298,6 +339,74 @@ func TestActionsViewViewPost(t *testing.T) {
 			tt.expectedTweaks(tt.expected)
 
 			assert.Equal(t, *tt.expected, actual)
+		})
+	}
+}
+
+func TestActionsViewRedirectToLatestAttempt(t *testing.T) {
+	unittest.PrepareTestEnv(t)
+
+	tests := []struct {
+		name         string
+		runIndex     int64
+		jobIndex     int64
+		expectedCode int
+		expectedURL  string
+	}{
+		{
+			name:        "no job index",
+			runIndex:    187,
+			jobIndex:    -1,
+			expectedURL: "https://try.gitea.io/user2/repo1/actions/runs/187/jobs/0/attempt/1",
+		},
+		{
+			name:        "job w/ 1 attempt",
+			runIndex:    187,
+			jobIndex:    0,
+			expectedURL: "https://try.gitea.io/user2/repo1/actions/runs/187/jobs/0/attempt/1",
+		},
+		{
+			name:        "job w/ multiple attempts",
+			runIndex:    187,
+			jobIndex:    2,
+			expectedURL: "https://try.gitea.io/user2/repo1/actions/runs/187/jobs/2/attempt/2",
+		},
+		{
+			name:         "run out-of-range",
+			runIndex:     5000,
+			jobIndex:     -1,
+			expectedCode: http.StatusNotFound,
+		},
+		// Odd behavior with an out-of-bound jobIndex -- defaults to the first job.  This is existing behavior
+		// documented in the getRunJobs internal helper which... seems not perfect for the redirect... but it's high
+		// risk to change and it's an OK user outcome to be redirected to something valid in the requested run.
+		{
+			name:        "job out-of-range",
+			runIndex:    187,
+			jobIndex:    500,
+			expectedURL: "https://try.gitea.io/user2/repo1/actions/runs/187/jobs/0/attempt/1",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, resp := contexttest.MockContext(t, "user2/repo1/actions/runs/0")
+			contexttest.LoadUser(t, ctx, 2)
+			contexttest.LoadRepo(t, ctx, 1)
+			ctx.SetParams(":run", fmt.Sprintf("%d", tt.runIndex))
+			if tt.jobIndex != -1 {
+				ctx.SetParams(":job", fmt.Sprintf("%d", tt.jobIndex))
+			}
+
+			RedirectToLatestAttempt(ctx)
+			if tt.expectedCode == 0 {
+				assert.Equal(t, http.StatusTemporaryRedirect, resp.Code)
+				url, err := resp.Result().Location()
+				require.NoError(t, err)
+				assert.Equal(t, tt.expectedURL, url.String())
+			} else {
+				assert.Equal(t, tt.expectedCode, resp.Code)
+			}
 		})
 	}
 }

Makefile
@@ -527,7 +527,7 @@ test: test-frontend test-backend
 .PHONY: test-backend
 test-backend:
 	@echo "Running go test with $(GOTESTFLAGS) -tags '$(TEST_TAGS)'..."
-	@$(GOTEST) $(GOTESTFLAGS) -tags='$(TEST_TAGS)' $(GO_TEST_PACKAGES)
+	@TZ=UTC $(GOTEST) $(GOTESTFLAGS) -tags='$(TEST_TAGS)' $(GO_TEST_PACKAGES)
 
 .PHONY: test-remote-cacher
 test-remote-cacher:
@@ -557,7 +557,7 @@ test-check:
 .PHONY: test\#%
 test\#%:
 	@echo "Running go test with $(GOTESTFLAGS) -tags '$(TEST_TAGS)'..."
-	@$(GOTEST) $(GOTESTFLAGS) -tags='$(TEST_TAGS)' -run $(subst .,/,$*) $(GO_TEST_PACKAGES)
+	@TZ=UTC $(GOTEST) $(GOTESTFLAGS) -tags='$(TEST_TAGS)' -run $(subst .,/,$*) $(GO_TEST_PACKAGES)
 
 coverage-merge:
 	rm -fr coverage/merged ; mkdir -p coverage/merged

models/actions/main_test.go
@@ -15,6 +15,10 @@ func TestMain(m *testing.M) {
 			"action_runner.yml",
 			"repository.yml",
 			"action_runner_token.yml",
+			"user.yml",
+			"action_run.yml",
+			"action_run_job.yml",
+			"action_task.yml",
 		},
 	})
 }

models/actions/run_job_test.go
@@ -3,9 +3,14 @@
 package actions
 
 import (
+	"fmt"
 	"testing"
 
+	"forgejo.org/models/db"
+	"forgejo.org/models/unittest"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestActionRunJob_ItRunsOn(t *testing.T) {
@@ -26,4 +31,42 @@ func TestActionRunJob_ItRunsOn(t *testing.T) {
 	actionJob.RunsOn = []string{}
 
 	assert.False(t, actionJob.ItRunsOn(agentLabels))
+}
+
+func TestActionRunJob_HTMLURL(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	tests := []struct {
+		id       int64
+		expected string
+	}{
+		{
+			id:       192,
+			expected: "https://try.gitea.io/user5/repo4/actions/runs/187/jobs/0/attempt/1",
+		},
+		{
+			id:       393,
+			expected: "https://try.gitea.io/user2/repo1/actions/runs/187/jobs/1/attempt/1",
+		},
+		{
+			id:       394,
+			expected: "https://try.gitea.io/user2/repo1/actions/runs/187/jobs/2/attempt/2",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("id=%d", tt.id), func(t *testing.T) {
+			var job ActionRunJob
+			has, err := db.GetEngine(t.Context()).Where("id=?", tt.id).Get(&job)
+			require.NoError(t, err)
+			require.True(t, has, "load ActionRunJob from fixture")
+
+			err = job.LoadAttributes(t.Context())
+			require.NoError(t, err)
+
+			url, err := job.HTMLURL(t.Context())
+			require.NoError(t, err)
+			assert.Equal(t, tt.expected, url)
+		})
+	}
 }

models/actions/task.go
@@ -148,6 +148,21 @@ func (task *ActionTask) GenerateToken() (err error) {
 	return err
 }
 
+// Retrieve all the attempts from the same job as the target `ActionTask`.  Limited fields are queried to avoid loading
+// the LogIndexes blob when not needed.
+func (task *ActionTask) GetAllAttempts(ctx context.Context) ([]*ActionTask, error) {
+	var attempts []*ActionTask
+	err := db.GetEngine(ctx).
+		Cols("id", "attempt", "status", "started").
+		Where("job_id=?", task.JobID).
+		Desc("attempt").
+		Find(&attempts)
+	if err != nil {
+		return nil, err
+	}
+	return attempts, nil
+}
+
 func GetTaskByID(ctx context.Context, id int64) (*ActionTask, error) {
 	var task ActionTask
 	has, err := db.GetEngine(ctx).Where("id=?", id).Get(&task)
@@ -155,6 +170,18 @@ func GetTaskByID(ctx context.Context, id int64) (*ActionTask, error) {
 		return nil, err
 	} else if !has {
 		return nil, fmt.Errorf("task with id %d: %w", id, util.ErrNotExist)
+	}
+
+	return &task, nil
+}
+
+func GetTaskByJobAttempt(ctx context.Context, jobID, attempt int64) (*ActionTask, error) {
+	var task ActionTask
+	has, err := db.GetEngine(ctx).Where("job_id=?", jobID).Where("attempt=?", attempt).Get(&task)
+	if err != nil {
+		return nil, err
+	} else if !has {
+		return nil, fmt.Errorf("task with job_id %d and attempt %d: %w", jobID, attempt, util.ErrNotExist)
 	}
 
 	return &task, nil

models/actions/task_test.go
@@ -1,0 +1,48 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package actions
+
+import (
+	"testing"
+
+	"forgejo.org/models/db"
+	"forgejo.org/models/unittest"
+	"forgejo.org/modules/timeutil"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestActionTask_GetAllAttempts(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	var task ActionTask
+	has, err := db.GetEngine(t.Context()).Where("id=?", 47).Get(&task)
+	require.NoError(t, err)
+	require.True(t, has, "load ActionTask from fixture")
+
+	allAttempts, err := task.GetAllAttempts(t.Context())
+	require.NoError(t, err)
+	require.Len(t, allAttempts, 3)
+	assert.EqualValues(t, 47, allAttempts[0].ID, "ordered by attempt, 1")
+	assert.EqualValues(t, 53, allAttempts[1].ID, "ordered by attempt, 2")
+	assert.EqualValues(t, 52, allAttempts[2].ID, "ordered by attempt, 3")
+
+	// GetAllAttempts doesn't populate all fields; so check expected fields from one of the records
+	assert.EqualValues(t, 3, allAttempts[0].Attempt, "read Attempt field")
+	assert.Equal(t, StatusRunning, allAttempts[0].Status, "read Status field")
+	assert.Equal(t, timeutil.TimeStamp(1683636528), allAttempts[0].Started, "read Started field")
+}
+
+func TestActionTask_GetTaskByJobAttempt(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	task, err := GetTaskByJobAttempt(t.Context(), 192, 2)
+	require.NoError(t, err)
+	assert.EqualValues(t, 192, task.JobID)
+	assert.EqualValues(t, 2, task.Attempt)
+
+	_, err = GetTaskByJobAttempt(t.Context(), 192, 100)
+	assert.ErrorContains(t, err, "task with job_id 192 and attempt 100: resource does not exist")
+}

commit 1e7ec058119f9fc41649b1ed6c488625bb146b5c

fix: loading action logs on a task that isn't fetched yet, fails when the job is fetched (#9293)
Discovered a regression caused by #9017.

Steps to reproduce:
- Disable the forgejo-runner that will pick up a workflow
- Trigger any workflow to run
- Through the Actions list, click on the new workflow that is pending a runner to fetch it
- You'll be redirected to /user/repo/actions/runs/73/jobs/0/attempt/0  (attempt = 0)
  - The UI will appear normal with the job "Waiting"...
- Startup the forgejo-runner to pick up the workflow
- The UI will begin to have errors:
    - JavaScript promise rejection: JSON.parse: unexpected keyword at line 1 column 1 of the JSON data. Open browser console to see more details. (5)

The cause is that the redirect to `/attempt/0` occurs for a job that hasn't been started, but once the job is started attempt 0 is not a valid attempt and errors will occur when polling for data.  This fix corrects the problem by redirecting to the attempt that will be present (attempt 1) when the job is fetched.

Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/9293
Reviewed-by: Earl Warren <earl-warren@noreply.codeberg.org>
Co-authored-by: Mathieu Fenniak <mathieu@fenniak.net>
Co-committed-by: Mathieu Fenniak <mathieu@fenniak.net>

models/actions/run_job.go
@@ -66,7 +66,14 @@ func (job *ActionRunJob) HTMLURL(ctx context.Context) (string, error) {
 		return "", fmt.Errorf("action_run_job: unable to find job on run: %d", job.ID)
 	}
 
-	return fmt.Sprintf("%s/actions/runs/%d/jobs/%d/attempt/%d", job.Run.Repo.HTMLURL(), job.Run.Index, jobIndex, job.Attempt), nil
+	attempt := job.Attempt
+	// If a job has never been fetched by a runner yet, it will have attempt 0 -- but this attempt will never have a
+	// valid UI since attempt is incremented to 1 if it is picked up by a runner.
+	if attempt == 0 {
+		attempt = 1
+	}
+
+	return fmt.Sprintf("%s/actions/runs/%d/jobs/%d/attempt/%d", job.Run.Repo.HTMLURL(), job.Run.Index, jobIndex, attempt), nil
 }
 
 func (job *ActionRunJob) Duration() time.Duration {

models/fixtures/action_run_job.yml
@@ -136,7 +136,7 @@
   commit_sha: 985f0301dba5e7b34be866819cd15ad3d8f508ee
   is_fork_pull_request: 0
   name: job_2
-  attempt: 1
+  attempt: 0
   job_id: job_2
   task_id: null
   status: 5

routers/web/repo/actions/view_test.go
@@ -386,6 +386,8 @@ func TestActionsViewRedirectToLatestAttempt(t *testing.T) {
 		jobIndex     int64
 		expectedCode int
 		expectedURL  string
+		userID       int64
+		repoID       int64
 	}{
 		{
 			name:        "no job index",
@@ -420,13 +422,31 @@ func TestActionsViewRedirectToLatestAttempt(t *testing.T) {
 			jobIndex:    500,
 			expectedURL: "https://try.gitea.io/user2/repo1/actions/runs/187/jobs/0/attempt/1",
 		},
+		// This ActionRunJob has Attempt: 0 and TaskID: null, which indicates its first run is pending pickup by a
+		// runner.  Should redirect to the attempt/1 since that's what it will be when it is running.
+		{
+			name:        "redirect to non-picked task",
+			userID:      2,
+			repoID:      4,
+			runIndex:    190,
+			jobIndex:    0,
+			expectedURL: "https://try.gitea.io/user5/repo4/actions/runs/190/jobs/0/attempt/1",
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx, resp := contexttest.MockContext(t, "user2/repo1/actions/runs/0")
-			contexttest.LoadUser(t, ctx, 2)
-			contexttest.LoadRepo(t, ctx, 1)
+			if tt.userID == 0 {
+				contexttest.LoadUser(t, ctx, 2)
+			} else {
+				contexttest.LoadUser(t, ctx, tt.userID)
+			}
+			if tt.repoID == 0 {
+				contexttest.LoadRepo(t, ctx, 1)
+			} else {
+				contexttest.LoadRepo(t, ctx, tt.repoID)
+			}
 			ctx.SetParams(":run", fmt.Sprintf("%d", tt.runIndex))
 			if tt.jobIndex != -1 {
 				ctx.SetParams(":job", fmt.Sprintf("%d", tt.jobIndex))