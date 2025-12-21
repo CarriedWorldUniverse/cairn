CarriedWorldUniverse/cairn

commit de143879e5f615d5f079fd39dad4e6dcf4bbfb79

Update module github.com/editorconfig-checker/editorconfig-checker/v2/cmd/editorconfig-checker to v3

templates/repo/actions/runs_list.tmpl
@@ -15,7 +15,7 @@
 					{{if .Title}}{{.Title}}{{else}}{{ctx.Locale.Tr "actions.runs.empty_commit_message"}}{{end}}
 				</a>
 				<div class="flex-item-body">
-					<b>{{if not $.CurWorkflow}}{{.WorkflowID}} {{end}}#{{.Index}}</b> - 
+					<b>{{if not $.CurWorkflow}}{{.WorkflowID}} {{end}}#{{.Index}}</b> -
 					{{- if .ScheduleID -}}
 						{{ctx.Locale.Tr "actions.runs.scheduled"}}
 					{{- else -}}

.forgejo/workflows/release-notes-assistant-milestones.yml
@@ -20,9 +20,9 @@ jobs:
 
       - name: apt install jq
         run: |
-         export DEBIAN_FRONTEND=noninteractive
-         apt-get update -qq
-         apt-get -q install -y -qq jq
+          export DEBIAN_FRONTEND=noninteractive
+          apt-get update -qq
+          apt-get -q install -y -qq jq
 
       - name: update open milestones
         run: |

.forgejo/workflows/release-notes-assistant.yml
@@ -30,9 +30,9 @@ jobs:
 
       - name: apt install jq
         run: |
-         export DEBIAN_FRONTEND=noninteractive
-         apt-get update -qq
-         apt-get -q install -y -qq jq
+          export DEBIAN_FRONTEND=noninteractive
+          apt-get update -qq
+          apt-get -q install -y -qq jq
 
       - name: release-notes-assistant preview
         run: |

Makefile
@@ -27,7 +27,7 @@ DIFF ?= diff --unified
 XGO_VERSION := go-1.21.x
 
 AIR_PACKAGE ?= github.com/air-verse/air@v1 # renovate: datasource=go
-EDITORCONFIG_CHECKER_PACKAGE ?= github.com/editorconfig-checker/editorconfig-checker/v2/cmd/editorconfig-checker@2.8.0 # renovate: datasource=go
+EDITORCONFIG_CHECKER_PACKAGE ?= github.com/editorconfig-checker/editorconfig-checker/v3/cmd/editorconfig-checker@v3.0.3 # renovate: datasource=go
 GOFUMPT_PACKAGE ?= mvdan.cc/gofumpt@v0.6.0 # renovate: datasource=go
 GOLANGCI_LINT_PACKAGE ?= github.com/golangci/golangci-lint/cmd/golangci-lint@v1.59.1 # renovate: datasource=go
 GXZ_PACKAGE ?= github.com/ulikunitz/xz/cmd/gxz@v0.5.11 # renovate: datasource=go

templates/base/alert.tmpl
@@ -19,5 +19,5 @@
 	</div>
 {{end}}
 {{if and (not .Flash.ErrorMsg) (not .Flash.SuccessMsg) (not .Flash.InfoMsg) (not .Flash.WarningMsg) (not .IsHTMX)}}
-     <div id="flash-message" hx-swap-oob="true"></div>
+	<div id="flash-message" hx-swap-oob="true"></div>
 {{end}}

templates/htmx/milestone_sidebar.tmpl
@@ -1,4 +1,4 @@
 <div id="insert-timeline" hx-swap-oob="beforebegin">
-		 {{template "repo/issue/view_content/comments" .}}
+	{{template "repo/issue/view_content/comments" .}}
 </div>
 {{template "repo/issue/view_content/sidebar/milestones" .}}

templates/org/menu.tmpl
@@ -6,7 +6,7 @@
 				{{if .RepoCount}}
 					<div class="ui small label">{{.RepoCount}}</div>
 				{{end}}
-        <span hidden test-name="repository-count">{{.RepoCount}}</span>
+				<span hidden test-name="repository-count">{{.RepoCount}}</span>
 			</a>
 			{{if .CanReadProjects}}
 			<a class="{{if .PageIsViewProjects}}active {{end}}item" href="{{$.Org.HomeLink}}/-/projects">

commit 96da60d5d9f15e27240409e25962a8791724af38

[v14.0/forgejo] fix(ui): add missing space before 'Commit' back (#10526)
**Backport:** https://codeberg.org/forgejo/forgejo/pulls/10521

Regression from de143879e5f615d5f079fd39dad4e6dcf4bbfb79

Co-authored-by: Beowulf <beowulf@beocode.eu>
Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/10526
Reviewed-by: Beowulf <beowulf@beocode.eu>
Co-authored-by: forgejo-backport-action <forgejo-backport-action@noreply.codeberg.org>
Co-committed-by: forgejo-backport-action <forgejo-backport-action@noreply.codeberg.org>

templates/repo/actions/runs_list.tmpl
@@ -16,7 +16,7 @@
 				</a>
 				<div class="flex-item-body">
 					<b>{{if not $.CurWorkflow}}{{.WorkflowID}} {{end}}#{{.Index}}</b> -
-					{{- if .ScheduleID -}}
+					{{if .ScheduleID -}}
 						{{ctx.Locale.Tr "actions.runs.scheduled"}}
 					{{- else -}}
 						{{ctx.Locale.Tr "actions.runs.commit"}}

tests/integration/actions_runs_list_test.go
@@ -1,0 +1,26 @@
+// Copyright 2025 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package integration
+
+import (
+	"net/http"
+	"testing"
+
+	"forgejo.org/tests"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestActionRunsList(t *testing.T) {
+	defer tests.PrepareTestEnv(t)()
+
+	req := NewRequest(t, "GET", "/user5/repo4/actions")
+	resp := MakeRequest(t, req, http.StatusOK)
+
+	htmlDoc := NewHTMLParser(t, resp.Body)
+
+	runSubLine := htmlDoc.Find(".run-list .flex-item-body").Text()
+	assert.Contains(t, runSubLine, "Commit")
+	assert.NotContains(t, runSubLine, "-Commit")
+}